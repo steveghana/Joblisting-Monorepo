aa22d51929c92bbeb6c4e7e512fa3b7560b9a404
steveghana <boatengstephen707@gmail.com>
2023-10-02 18:47:23 +0000

    edit sidebar

--- app/web/src/components/layouts/SidebarLayout/index.tsx
+++ app/web/src/components/layouts/SidebarLayout/index.tsx
@@ -1,16 +1,50 @@
 import { FC, ReactNode } from "react";
-import { Box, alpha, lighten, useTheme } from "@mui/material";
+import { Box, alpha, lighten, styled, useTheme } from "@mui/material";
 // import { Outlet } from 'react-router-dom';
 
 import Sidebar from "./Sidebar";
 // import Header from "./Header";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { drawerWidth } from "store/constant";
 
 interface SidebarLayoutProps {
   children?: ReactNode;
 }
-
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    ...theme.typography.body1,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    transition: theme.transitions.create(
+      "margin",
+      open
+        ? {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }
+        : {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }
+    ),
+    [theme.breakpoints.up("md")]: {
+      marginLeft: open ? 0 : -(drawerWidth - 20),
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    [theme.breakpoints.down("md")]: {
+      marginLeft: "20px",
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: "16px",
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "10px",
+      width: `calc(100% - ${drawerWidth}px)`,
+      padding: "16px",
+      marginRight: "10px",
+    },
+  })
+);
 const SidebarLayout: FC<SidebarLayoutProps> = () => {
   const theme = useTheme();
 
