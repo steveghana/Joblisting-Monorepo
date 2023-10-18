d8d5c5f5e27591439ae2062f232a21710d88b9cf
steveghana <boatengstephen707@gmail.com>
2023-10-18 13:44:11 +0000

    add user details information to edit

--- app/web/src/content/applications/Users/settings/EditProfileTab.tsx
+++ app/web/src/content/applications/Users/settings/EditProfileTab.tsx
@@ -6,188 +6,212 @@ import {
   Box,
   Divider,
   Button,
+  TextField,
+  FormHelperText,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
 } from "@mui/material";
+import { gridSpacing } from "../../../../store/constant";
 
 import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
 import DoneTwoToneIcon from "@mui/icons-material/DoneTwoTone";
 import Text from "../../../../components/Text";
 import Label from "../../../../components/Label";
-
-function EditProfileTab() {
+import SubCard from "../../../../ui-component/cards/SubCard";
+import { Formik } from "formik";
+import { themeTypography } from "../../../../themes/schemes/typography";
+import AnimateButton from "../../../../ui-component/extended/AnimateButton";
+import CustomButton from "../../../../components/button";
+import MainCard from "../../../../ui-component/cards/MainCard";
+function EditProfileTab({ ...others }) {
+  // const []
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <Card>
-          <Box
-            p={3}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              firstName: "",
+              lastName: "",
+              role: "Ceo" || "Marketing" || "Recruitment" || "Developer",
+              submit: null,
+            }}
+            onSubmit={async (values, setters) => {
+              console.log(values, "from submitting");
+              // await register(values, setters, scriptedRef);
+            }}
           >
-            <Box>
-              <Typography variant="h4" gutterBottom>
-                Personal Details
-              </Typography>
-              <Typography variant="subtitle2">
-                Manage informations related to your personal details
-              </Typography>
-            </Box>
-            <Button variant="text" startIcon={<EditTwoToneIcon />}>
-              Edit
-            </Button>
-          </Box>
-          <Divider />
-          <CardContent sx={{ p: 4 }}>
-            <Typography variant="subtitle2">
-              <Grid container spacing={0}>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Name:
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values,
+            }) => (
+              <form noValidate onSubmit={handleSubmit} {...others}>
+                <MainCard>
+                  <Grid container>
+                    <Grid item xs={12} sm={4} mr={2}>
+                      <SubCard title="Personal information">
+                        <Grid container spacing={2}>
+                          <Grid item xs={12} sm={6}>
+                            <TextField
+                              fullWidth
+                              label="First Name"
+                              margin="normal"
+                              value={values.firstName}
+                              name="firstName"
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                              type="text"
+                              defaultValue=""
+                              sx={{ ...themeTypography.customInput }}
+                            />
+                          </Grid>
+                          <Grid item xs={12} sm={6}>
+                            <TextField
+                              fullWidth
+                              value={values.lastName}
+                              label="Last Name"
+                              margin="normal"
+                              name="lastName"
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                              type="text"
+                              defaultValue=""
+                              sx={{ ...themeTypography.customInput }}
+                            />
+                          </Grid>
+                        </Grid>
+                        <FormControl
+                          fullWidth
+                          error={Boolean(touched.email && errors.email)}
+                          sx={{ ...themeTypography.customInput }}
+                        >
+                          <InputLabel htmlFor="outlined-adornment-email-register">
+                            Email Address / Username
+                          </InputLabel>
+                          <OutlinedInput
+                            id="outlined-adornment-email-register"
+                            type="email"
+                            value={values.email}
+                            name="email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            inputProps={{}}
+                          />
+                          {touched.email && errors.email && (
+                            <FormHelperText
+                              error
+                              id="standard-weight-helper-text--register"
+                            >
+                              {errors.email}
+                            </FormHelperText>
+                          )}
+                        </FormControl>
+                      </SubCard>
+                    </Grid>
+                  </Grid>
+                  <Grid container lg={8} xs={12} sm={12} spacing={gridSpacing}>
+                    <Grid item xs={12} sm={12}>
+                      <SubCard title="Personal information">
+                        <Grid container spacing={2}>
+                          <Grid item xs={12} sm={6}>
+                            <TextField
+                              fullWidth
+                              label="First Name"
+                              margin="normal"
+                              value={values.firstName}
+                              name="firstName"
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                              type="text"
+                              defaultValue=""
+                              sx={{ ...themeTypography.customInput }}
+                            />
+                          </Grid>
+                          <Grid item xs={12} sm={6}>
+                            <TextField
+                              fullWidth
+                              value={values.lastName}
+                              label="Last Name"
+                              margin="normal"
+                              name="lastName"
+                              onBlur={handleBlur}
+                              onChange={handleChange}
+                              type="text"
+                              defaultValue=""
+                              sx={{ ...themeTypography.customInput }}
+                            />
+                          </Grid>
+                        </Grid>
+                        <FormControl
+                          fullWidth
+                          error={Boolean(touched.email && errors.email)}
+                          sx={{ ...themeTypography.customInput }}
+                        >
+                          <InputLabel htmlFor="outlined-adornment-email-register">
+                            Email Address / Username
+                          </InputLabel>
+                          <OutlinedInput
+                            id="outlined-adornment-email-register"
+                            type="email"
+                            value={values.email}
+                            name="email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            inputProps={{}}
+                          />
+                          {touched.email && errors.email && (
+                            <FormHelperText
+                              error
+                              id="standard-weight-helper-text--register"
+                            >
+                              {errors.email}
+                            </FormHelperText>
+                          )}
+                        </FormControl>
+                      </SubCard>
+                    </Grid>
+                  </Grid>
+                </MainCard>
+
+                {errors.submit && (
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{ mt: 3 }}
+                  >
+                    <FormHelperText error>
+                      {errors.submit as string}
+                    </FormHelperText>
                   </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>Craig Donin</b>
-                  </Text>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Date of birth:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>15 March 1977</b>
-                  </Text>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Address:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Box sx={{ maxWidth: { xs: "auto", sm: 300 } }}>
-                    <Text color="black">
-                      1749 High Meadow Lane, SEQUOIA NATIONAL PARK, California,
-                      93262
-                    </Text>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <Box
-            p={3}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Box>
-              <Typography variant="h4" gutterBottom>
-                Account Settings
-              </Typography>
-              <Typography variant="subtitle2">
-                Manage details related to your account
-              </Typography>
-            </Box>
-            <Button variant="text" startIcon={<EditTwoToneIcon />}>
-              Edit
-            </Button>
-          </Box>
-          <Divider />
-          <CardContent sx={{ p: 4 }}>
-            <Typography variant="subtitle2">
-              <Grid container spacing={0}>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Language:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>English (US)</b>
-                  </Text>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Timezone:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>GMT +2</b>
-                  </Text>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Account status:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Label color="success">
-                    <DoneTwoToneIcon fontSize="small" />
-                    <b>Active</b>
-                  </Label>
-                </Grid>
-              </Grid>
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <Box
-            p={3}
-            display="flex"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Box>
-              <Typography variant="h4" gutterBottom>
-                Email Addresses
-              </Typography>
-              <Typography variant="subtitle2">
-                Manage details related to your associated email addresses
-              </Typography>
-            </Box>
-            <Button variant="text" startIcon={<EditTwoToneIcon />}>
-              Edit
-            </Button>
-          </Box>
-          <Divider />
-          <CardContent sx={{ p: 4 }}>
-            <Typography variant="subtitle2">
-              <Grid container spacing={0}>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Email ID:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>example@demo.com</b>
-                  </Text>
-                  <Box pl={1} component="span">
-                    <Label color="success">Primary</Label>
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={4} md={3} textAlign={{ sm: "right" }}>
-                  <Box pr={3} pb={2}>
-                    Email ID:
-                  </Box>
-                </Grid>
-                <Grid item xs={12} sm={8} md={9}>
-                  <Text color="black">
-                    <b>demo@example.com</b>
-                  </Text>
-                </Grid>
-              </Grid>
-            </Typography>
-          </CardContent>
+                )}
+
+                <Box sx={{ mt: 2 }}>
+                  <AnimateButton>
+                    <CustomButton
+                      disableElevation
+                      disabled={isSubmitting}
+                      fullWidth
+                      size="large"
+                      variant="contained"
+                      text="Sign up"
+                      type="submit"
+
+                      // color="secondary"
+                    />
+                  </AnimateButton>
+                </Box>
+              </form>
+            )}
+          </Formik>
         </Card>
       </Grid>
     </Grid>
