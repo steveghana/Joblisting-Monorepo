8cfd972d86369e2ddfad3031f36f05b0621a942c
steveghana <boatengstephen707@gmail.com>
2023-11-02 08:38:38 +0000

    validate personal info

--- app/web/src/views/dashboard/Roles/ApplicationForm/Personalinfo.tsx
+++ app/web/src/views/dashboard/Roles/ApplicationForm/Personalinfo.tsx
@@ -1,73 +1,86 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import {
   TextField,
   Button,
   useTheme,
   useMediaQuery,
-  OutlinedInput,
   Box,
   Typography,
+  FormHelperText,
 } from "@mui/material";
 import CustomButton from "../../../../components/button";
 
 const PersonalInfoForm = ({ onSubmit }) => {
   const theme = useTheme();
-
   const matchUpMd = useMediaQuery(theme.breakpoints.up("md"));
+
+  const formFields = [
+    { name: "name", label: "Name" },
+    { name: "email", label: "Email" },
+    { name: "phoneNumber", label: "Phone Number" },
+  ];
 
   return (
     <>
       <Typography variant="h4">Personal Info</Typography>
       <Formik
         initialValues={{ name: "", email: "", phoneNumber: "" }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email("Must be a valid email")
+            .max(255)
+            .required("Email is required"),
+          name: Yup.string()
+            .max(255)
+            .min(2)
+            .required("Please enter a valid name"),
+          phoneNumber: Yup.string()
+            .matches(/^[0-9]{8,15}$/, "Please enter a valid phone number")
+            .required("Please enter your phone number"),
+        })}
         onSubmit={onSubmit}
       >
-        {({
-          values,
-          errors,
-          handleBlur,
-          handleChange,
-          handleSubmit,
-          isSubmitting,
-          touched,
-        }) => (
+        {({ values, errors, touched, handleSubmit, isSubmitting }) => (
           <Form>
-            <Field
-              name="name"
-              value={values.name}
-              as={TextField}
-              label="Name"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-            />
-            <Field
-              name="email"
-              as={TextField}
-              label="Email"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-            />
-            <Field
-              name="phoneNumber"
-              as={TextField}
-              label="Phone Number"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-            />
-            <Box width={"100%"} display={"flex"} justifyContent={"center"}>
-              <CustomButton
-                fullWidth={matchUpMd ? false : true}
-                text="Next"
-                type="submit"
+            {formFields.map((item) => (
+              <Field
+                key={item.name}
+                name={item.name}
+                as={TextField}
+                label={item.label}
+                variant="outlined"
+                fullWidth
+                margin="normal"
               />
+            ))}
+            {/* {formFields.map(({ name, label }) => (
+              <Field
+                key={name}
+                name={name}
+                as={TextField}
+                label={label}
+                variant="outlined"
+                fullWidth
+                margin="normal"
+              />
+            )} */}
+
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              sx={{ mt: 3 }}
+            >
+              <FormHelperText error>
+                {errors.name || errors.email || errors.phoneNumber}
+              </FormHelperText>
             </Box>
-            {/* <Button type="submit" variant="contained" color="primary">
-            Next
-          </Button> */}
+
+            <Box width="100%" display="flex" justifyContent="center">
+              <CustomButton fullWidth={!matchUpMd} text="Next" type="submit" />
+            </Box>
           </Form>
         )}
       </Formik>
