416240c28bc28b2cd6628bed521ccd43f2057ba5
steveghana <boatengstephen707@gmail.com>
2023-11-08 14:03:03 +0000

    create new editor for client description

--- app/web/src/content/applications/Users/settings/EditorToolbar.tsx
+++ app/web/src/content/applications/Users/settings/EditorToolbar.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
-
-import Box, { BoxProps } from "@mui/joy/Box";
-import Select from "@mui/joy/Select";
-import Option from "@mui/joy/Option";
-import IconButton from "@mui/joy/IconButton";
-
+import {
+  Box,
+  BoxProps,
+  IconButton,
+  MenuItem,
+  Select,
+  TextareaAutosize,
+  Typography,
+} from "@mui/material";
 import FormatBoldRoundedIcon from "@mui/icons-material/FormatBoldRounded";
 import FormatItalicRoundedIcon from "@mui/icons-material/FormatItalicRounded";
-import StrikethroughSRoundedIcon from "@mui/icons-material/StrikethroughSRounded";
 import FormatListBulletedRoundedIcon from "@mui/icons-material/FormatListBulletedRounded";
 import { useMediaQuery, useTheme } from "@mui/material";
+import { useState } from "react";
 
 export default function EditorToolbar({ sx, ...props }: BoxProps) {
   const theme = useTheme();
@@ -21,6 +24,8 @@ export default function EditorToolbar({ sx, ...props }: BoxProps) {
       sx={[
         {
           display: "flex",
+          my: 2,
+          alignItems: "flex-end",
           gap: 0.5,
           "& > button": { "--Icon-fontSize": "16px" },
           flexDirection: {
@@ -33,26 +38,52 @@ export default function EditorToolbar({ sx, ...props }: BoxProps) {
         ...(Array.isArray(sx) ? sx : [sx]),
       ]}
     >
-      <Select size="sm" defaultValue="1" sx={{ minWidth: 160 }}>
-        <Option value="1">Normal text</Option>
-        <Option value="2" sx={{ fontFamily: "code" }}>
+      <Select defaultValue="1" sx={{ minWidth: 160 }}>
+        <MenuItem value="1">Normal text</MenuItem>
+        <MenuItem value="2" sx={{ fontFamily: "code" }}>
           Code text
-        </Option>
+        </MenuItem>
       </Select>
       <Box>
-        <IconButton size="sm" variant="plain" color="neutral">
+        <IconButton size="small" color="default" sx={{ outline: "none" }}>
           <FormatBoldRoundedIcon />
         </IconButton>
-        <IconButton size="sm" variant="plain" color="neutral">
+        <IconButton size="small" color="default">
           <FormatItalicRoundedIcon />
         </IconButton>
-        <IconButton size="sm" variant="plain" color="neutral">
-          <StrikethroughSRoundedIcon />
-        </IconButton>
-        <IconButton size="sm" variant="plain" color="neutral">
+        <IconButton size="small" color="default">
           <FormatListBulletedRoundedIcon />
         </IconButton>
       </Box>
     </Box>
+  );
+}
+
+export function LargeTextField() {
+  const [text, setText] = useState("");
+  const wordLimit = 300;
+  const words = text.trim().split(/\s+/).filter(Boolean).length;
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <EditorToolbar />
+      <TextareaAutosize
+        minRows={1}
+        style={{ width: "100%", minHeight: "150px", padding: 1 }}
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your description here..."
+      />
+      <Typography
+        variant="body2"
+        color={words > wordLimit ? "error" : "textSecondary"}
+      >
+        {words} words ({wordLimit - words} words left)
+      </Typography>
+    </div>
   );
 }
