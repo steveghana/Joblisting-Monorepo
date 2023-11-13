50c957497160f0d88ce02020f1bfe9e801738d89
steveghana <boatengstephen707@gmail.com>
2023-11-13 13:35:33 +0000

    add table validations

---
+++ app/web/src/lib/tablevalidate.ts
@@ -0,0 +1,21 @@
+import { IClient } from "../types/client";
+
+// validation.ts
+export const validateRequired = (value: string) => !!value.length;
+export const validateEmail = (email: string) =>
+  !!email.length &&
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
+export function validateUser(user: IClient) {
+  return {
+    firstName: !validateRequired(user.firstName)
+      ? "First Name is Required"
+      : "",
+    lastName: !validateRequired(user.lastName) ? "Last Name is Required" : "",
+    email: !validateEmail(user.email) ? "Incorrect Email Format" : "",
+  };
+}
