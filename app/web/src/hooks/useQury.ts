96b7be8816b1a77c950c6fb835242b0f490c63d8
steveghana <boatengstephen707@gmail.com>
2023-11-13 13:35:10 +0000

    add query hooks

---
+++ app/web/src/hooks/useQury.ts
@@ -0,0 +1,79 @@
+// hooks.ts
+import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
+// import { data } from "./data";
+// import { validateUser } from "./validation";
+import { IClient } from "../types/client";
+import { data } from "../views/dashboard/clients/Details/clientForm/data";
+
+export function useCreateUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (user: IClient) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return Promise.resolve();
+    },
+    onMutate: (newUserInfo: IClient) => {
+      queryClient.setQueryData(
+        ["users"],
+        (prevUsers: any) =>
+          [
+            ...prevUsers,
+            {
+              ...newUserInfo,
+              id: (Math.random() + 1).toString(36).substring(7),
+            },
+          ] as IClient[]
+      );
+    },
+  });
+}
+
+export function useGetUsers() {
+  return useQuery<IClient[]>({
+    queryKey: ["users"],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return Promise.resolve(data);
+    },
+    refetchOnWindowFocus: false,
+  });
+}
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (user: IClient) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return Promise.resolve();
+    },
+    onMutate: (newUserInfo: IClient) => {
+      queryClient.setQueryData(
+        ["users"],
+        (prevUsers: any) =>
+          prevUsers?.map((prevUser: IClient) =>
+            prevUser.email === newUserInfo.email ? newUserInfo : prevUser
+          )
+      );
+    },
+  });
+}
+
+export function useDeleteUser() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (userId: string) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      return Promise.resolve();
+    },
+    onMutate: (userId: string) => {
+      queryClient.setQueryData(
+        ["users"],
+        (prevUsers: any) =>
+          prevUsers?.filter((user: IClient) => user.email !== userId)
+      );
+    },
+  });
+}
