ce1b04614c107ab1936fb3f521c7fe070b6f691b
steveghana <boatengstephen707@gmail.com>
2023-11-13 13:36:05 +0000

    add client data types

---
+++ app/web/src/types/client.ts
@@ -0,0 +1,10 @@
+export type IClient = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  jobTitle: string;
+  salary: number;
+  startDate: string;
+  signatureCatchPhrase: string;
+  avatar: string;
+};
