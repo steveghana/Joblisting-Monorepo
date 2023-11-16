e18d5c598b1afb35ce715e6d0d32ad4c51418eec
steveghana <boatengstephen707@gmail.com>
2023-11-16 15:22:36 +0000

    add docs for configurations

--- app/web/src/store/index.ts
+++ app/web/src/store/index.ts
@@ -1,20 +1,64 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userApi } from "./services/userAuthService"; // Import your user service
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { USER_API_KEY, userApi } from "./services/userAuthService";
 import customizationReducer from "./customizationReducer";
-import { clientApi } from "./services/ClientServce";
-import { devApi } from "./services/DevsService";
-import { roleApi } from "./services/roleService";
+import { Reducer } from "redux";
+import { CLIENT_API_KEY, clientApi } from "./services/ClientServce";
+import { DEV_API_KEY, devApi } from "./services/DevsService";
+import { ROLE_API_KEY, roleApi } from "./services/roleService";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import { RESET_STATE_ACTION_TYPE } from "./actions";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { unauthenticatedMiddleware } from "./middleware/unauthenticatedMiddleware";
+
+const reducers = {
+  [USER_API_KEY]: userApi.reducer,
+  [CLIENT_API_KEY]: clientApi.reducer,
+  [DEV_API_KEY]: devApi.reducer,
+  [ROLE_API_KEY]: roleApi.reducer,
+  customization: customizationReducer,
+};
+
+const combinedReducer = combineReducers<typeof reducers>(reducers);
+
+// Create a custom rootReducer that resets state on a specific action type
+export const reducer: Reducer<RootState> = (state, action) => {
+  if (action.type === RESET_STATE_ACTION_TYPE) {
+    state = {} as RootState; // Just reset state to an empty object
+  }
+
+  return combinedReducer(state, action);
+};
+
 const store = configureStore({
-  reducer: {
-    [userApi.reducerPath]: userApi.reducer,
-    [clientApi.reducerPath]: clientApi.reducer,
-    [devApi.reducerPath]: devApi.reducer,
-    [roleApi.reducerPath]: roleApi.reducer,
-    customization: customizationReducer,
-    // Add other reducers if needed
-  },
+  reducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat([
+      unauthenticatedMiddleware,
+      userApi.middleware,
+      clientApi.middleware,
+      roleApi.middleware,
+      devApi.middleware,
+    ]),
 });
-export type RootState = ReturnType<typeof store.getState>;
+
+// Create a Redux Persist store to persist the state in the lifeCycle of the app
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof combinedReducer>; // Type for the root state
+export const useTypedDispatch = () => useDispatch<AppDispatch>(); // Type inference for dispatch
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector; // Type inference for useSelector
+
 export default store;
--- app/web/src/store/middleware/unauthenticatedMiddleware.ts
+++ app/web/src/store/middleware/unauthenticatedMiddleware.ts
@@ -1,10 +1,13 @@
-import { isRejectedWithValue, Middleware } from '@reduxjs/toolkit';
-import { resetStateAction } from '../actions/resetState';
+import { isRejectedWithValue, Middleware } from "@reduxjs/toolkit";
+import { resetStateAction } from "../actions";
 
-export const unauthenticatedMiddleware: Middleware = ({ dispatch }) => (next) => (action) => {
-  if (isRejectedWithValue(action) && action.payload.status === 401) {
-    dispatch(resetStateAction());
-  }
+export const unauthenticatedMiddleware: Middleware =
+  ({ dispatch }) =>
+  (next) =>
+  (action) => {
+    if (isRejectedWithValue(action) && action.payload.status === 401) {
+      dispatch(resetStateAction());
+    }
 
-  return next(action);
-};
+    return next(action);
+  };
