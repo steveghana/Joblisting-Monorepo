3997d4125874e72a077c710adaff750283fdfb0b
steveghana <boatengstephen707@gmail.com>
2023-11-27 14:56:55 +0000

    updte dto and types for client, role and job informations

--- app/api/src/apps/roles/dto/create-role.dto.ts
+++ app/api/src/apps/roles/dto/create-role.dto.ts
@@ -13,54 +13,112 @@ import {
   IsNumber,
 } from 'class-validator';
 import { IRole } from '@/types/role';
+import { Type } from 'class-transformer';
 
-export class CreateRoleDto {
-  @IsNotEmpty()
-  @IsNumber()
-  clientId: number;
-  @IsNotEmpty()
-  @IsString()
-  description: string;
-  // developer: any;
-  id?: number;
+export class ProjectDetailsDto {
+  // Assuming DevsNeeded is a string
   @IsNotEmpty()
   @IsString()
   devsNeeded: string;
+  @IsNotEmpty()
+  @IsString()
+  aboutTheProject: string;
   @IsOptional()
   @IsString()
-  dataContent: string;
-  @IsNotEmpty()
+  title: string;
+  @IsOptional()
   @IsString()
-  durationForEmployment: string;
-  @IsNotEmpty()
-  @IsString()
-  experience: string;
+  numOfEmployees: string;
+
+  // Assuming methodology is a string
   @IsNotEmpty()
   @IsString()
   methodology: string;
   @IsNotEmpty()
   @IsString()
-  numOfEmployees: string;
+  experience: string;
+
   @IsOptional()
   @IsString()
-  hiringRole: string;
+  roleName: string;
+  // Assuming testingQA is a string
   @IsNotEmpty()
   @IsString()
-  whenToStart: string;
+  communicationPreferences: string;
+
+  constructor(data: ProjectDetailsDto) {
+    Object.assign(this, data);
+  }
+}
+
+export class RoleInfoDto {
+  // Assuming durationForEmployment is a string
+  @IsNotEmpty()
+  @IsString()
+  durationForEmployment: string;
+  @IsNotEmpty()
+  @IsString()
+  roleName: string;
   @IsNotEmpty()
   @IsArray()
   selectedSkills: string[];
+  // Assuming whenToStart is a string
   @IsNotEmpty()
   @IsString()
-  title: string;
+  whenToStart: string;
+  @IsOptional()
+  @IsString()
+  salary: string;
   @IsNotEmpty()
   @IsString()
   employmentType: string;
+
+  @IsOptional()
+  @IsString()
+  roleCategory: string;
+  @IsOptional()
+  @IsString()
+  postedDate: Date;
   @IsNotEmpty()
   @IsString()
-  aboutTheProject: string;
-  // vacancy_status: 'Open' | 'Closed';
-  constructor(data: IRole) {
+  tasks: string[];
+  @IsOptional()
+  @IsString()
+  description: string[];
+  constructor(data: JobInfo) {
     Object.assign(this, data);
   }
+}
+
+export interface JobInfo {
+  description: string[];
+  id: string;
+  role: IRole;
+  roleType: string;
+  whenToStart: string;
+  employmentType: string;
+  jobtype: string;
+  salary: string;
+  roleCategory: string;
+  postedDate: Date;
+  roleName: string;
+  tasks: string[]; // Array of tasks
+}
+
+export class CreateRoleDto {
+  @IsNotEmpty()
+  @IsString()
+  clientId: number;
+
+  id?: string;
+  @Type(() => ProjectDetailsDto)
+  @ValidateNested()
+  @IsNotEmpty()
+  'Project Details': ProjectDetailsDto;
+
+  @Type(() => RoleInfoDto)
+  @ValidateNested()
+  @IsNotEmpty()
+  'Role Info': RoleInfoDto;
+  // vacancy_status: 'Open' | 'Closed';
 }
