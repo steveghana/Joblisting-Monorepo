f382c3d16aa1222ba94920f48d86880cbbda747d
steveghana <boatengstephen707@gmail.com>
2023-11-30 22:39:20 +0000

    Batch Committing file: C:\Users\boate\Desktop\Projects\Remote job listing\commit_files.bat

--- commit_files.bat
+++ commit_files.bat
@@ -1,10 +1,10 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
+REM Iterate through all files in the directory and its subdirectories
 for /r %%i in (*) do (
     git add "%%i"
-   
-    git commit -m "Batch Committing file:update %%i"
-
+    git commit -m "Batch Committing file: %%i"
 )
 
 git push
