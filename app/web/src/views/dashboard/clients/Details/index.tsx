717d84c5bd9590e99858ae139a3f7b37d00d5db1
steveghana <boatengstephen707@gmail.com>
2023-11-30 22:49:16 +0000

    Batch Committing file: app/web/src/views/dashboard/clients/Details/index.tsx

--- app/web/src/views/dashboard/clients/Details/index.tsx
+++ app/web/src/views/dashboard/clients/Details/index.tsx
@@ -1,201 +1,188 @@
-import React from "react";
 import {
-  Typography,
-  Grid,
-  Paper,
   Avatar,
-  Card,
-  CardContent,
-  Container,
-  Box,
   Divider,
+  Grid,
+  IconButton,
+  Tab,
+  Tabs,
+  Tooltip,
+  Typography,
+  styled,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
+import React, { ChangeEvent } from "react";
+import SubCard from "../../../../components/SubCard";
+import { Box } from "@mui/system";
+import {
+  ArrowBackTwoTone,
+  Email,
+  Facebook,
+  LocalActivity,
+  Phone,
+  Pinterest,
+  Title,
+  VerifiedUser,
+  WhatsApp,
+} from "@mui/icons-material";
+import { themePalette } from "../../../../themes/schemes/palette";
+import ClientDetailsOverview from "./overview";
+import { useNavigate, useParams } from "react-router";
+import { useGetClientQuery } from "../../../../store/services/ClientServce";
+import FullscreenProgress from "../../../../components/FullscreenProgress/FullscreenProgress";
+const TabsWrapper = styled(Tabs)(
+  () => `
+    .MuiTabs-scrollableX {
+      overflow-x: auto !important;
+    }
+`
+);
+type Tabstring = "projects" | "tasks" | "devs";
+const ClientDetails = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-interface DeveloperRoleProps {
-  role: string;
-  description: string;
-}
-
-const DeveloperRole: React.FC<DeveloperRoleProps> = ({ role, description }) => {
-  return (
-    <Box className="developer-role">
-      <Typography variant="h6">{role}</Typography>
-      <Typography>{description}</Typography>
-    </Box>
-  );
-};
-
-interface TeamMemberProps {
-  name: string;
-  role: string;
-  profile: string;
-}
-
-const TeamMember: React.FC<TeamMemberProps> = ({ name, role, profile }) => {
-  return (
-    <Card
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        padding: 2,
-        backgroundColor: "#f5f5f5",
-        transition: "0.3s",
-        "&:hover": {
-          backgroundColor: "#ececec",
-        },
-      }}
-      elevation={3}
-    >
-      <Avatar alt={name} src={profile} />
-      <CardContent>
-        <Typography variant="h6">{name}</Typography>
-        <Typography>{role}</Typography>
-      </CardContent>
-    </Card>
-  );
-};
-
-const ClientDetailsPage: React.FC<{ data: /* FormDataState */ any }> = ({
-  data,
-}) => {
-  const paperStyle: React.CSSProperties = {
-    padding: "16px",
-    marginBottom: "16px",
+  const isLargerScreen = useMediaQuery(theme.breakpoints.up("md"));
+  const [currentTab, setCurrentTab] = React.useState<Tabstring>("projects");
+  const { data, isError, isLoading, isFetching } = useGetClientQuery({
+    id,
+  });
+  const tabs = [
+    { value: "projects", label: "Project" },
+    { value: "tasks", label: "Tasks" },
+    { value: "devs", label: "Developers" },
+  ];
+  const handleTabsChange = (event: ChangeEvent<{}>, value: Tabstring): void => {
+    //dont propagate to the drawer component
+    event.stopPropagation();
+    setCurrentTab(value);
   };
-
+  if (isLoading || isFetching) {
+    return <FullscreenProgress />;
+  }
   return (
-    <Container>
-      <Typography variant="h4" align="center" style={{ marginBottom: "16px" }}>
-        Client Details
-      </Typography>
-
-      {/* Client Info */}
-      <Paper elevation={3} style={paperStyle}>
-        <Typography variant="h5" gutterBottom>
-          Client Information
-        </Typography>
-        <Divider />
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="subtitle1">
-              <strong>Name:</strong> {data["Client Info"].name}
+    <Grid sx={{ background: "white", p: 1 }}>
+      <Tooltip arrow placement="top" title="Go back">
+        <IconButton
+          color="primary"
+          onClick={() => navigate(-1)}
+          sx={{ p: 2, mr: 2 }}
+        >
+          <ArrowBackTwoTone />
+        </IconButton>
+      </Tooltip>
+      <SubCard>
+        <Grid display={"flex"} mb={3} mt={1}>
+          <Box
+            borderRight={`1px solid ${themePalette.primary.light}`}
+            display={"flex"}
+            flexDirection={"column"}
+            flexWrap={"wrap"}
+            maxWidth={"30%"}
+            alignItems={"center"}
+            gap={1.5}
+            p={1}
+          >
+            <Avatar sx={{ width: 56, height: 56 }} />
+            <Typography>Croxino Technologies</Typography>
+            <Typography textAlign={"center"} variant="caption">
+              A leading technology company specializing in innovative solutions.
+              Our mission is to transform industries through cutting-edge
+              technology.
             </Typography>
+            <Box display={"flex"} gap={1} alignItems={"center"}>
+              <Facebook />
+              <Pinterest />
+              <WhatsApp />
+            </Box>
+          </Box>
+          <Grid display={"flex"} flexDirection={"column"} gap={1} p={1}>
+            <Box
+              display={"flex"}
+              flexWrap={"wrap"}
+              justifyContent={"space-between"}
+              gap={1}
+              alignItems={"center"}
+            >
+              <Grid display={"flex"} gap={1}>
+                <VerifiedUser color="disabled" />
+                <Box>
+                  <Typography variant="caption">Contact name</Typography>
+                  <Typography>Marcus Lane</Typography>
+                </Box>
+              </Grid>
+              <Grid display={"flex"} gap={1}>
+                <Email color="disabled" />
+                <Box>
+                  <Typography variant="caption">Email Address</Typography>
+                  <Typography>Steve@gmail.com</Typography>
+                </Box>
+              </Grid>
+              <Grid display={"flex"} gap={1}>
+                <Phone color="disabled" />
+                <Box>
+                  <Typography variant="caption">Phone No.</Typography>
+                  <Typography>233554544455</Typography>
+                </Box>
+              </Grid>
+              <Box display={"flex"} gap={1}>
+                <Title color="disabled" />
+                <Box>
+                  <Typography variant="caption">Title</Typography>
+                  <Typography>Ceo</Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Box>
+              <Grid display={"flex"} gap={1}>
+                <LocalActivity color="disabled" />
+                <Box>
+                  <Typography variant="caption">Address</Typography>
+                  <Typography>D stree, Hudson Bay lane , Florida</Typography>
+                </Box>
+              </Grid>
+            </Box>
+            {/* <Box></Box> */}
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography variant="subtitle1">
-              <strong>Email:</strong> {data["Client Info"].email}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography variant="subtitle1">
-              <strong>Phone Number:</strong> {data["Client Info"].phoneNumber}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Typography variant="subtitle1">
-              <strong>Company Name:</strong> {data["Client Info"].companyName}
-            </Typography>
-          </Grid>
-          {/* Add other fields as needed */}
         </Grid>
-      </Paper>
-
-      {/* Project Details */}
-      <Paper elevation={3} style={paperStyle}>
-        <Typography variant="h5" gutterBottom>
-          Project Details
-        </Typography>
-        <Divider />
-        <Grid container spacing={2}>
-          {/* Add fields from Project Details */}
+      </SubCard>
+      <Divider sx={{ m: 2 }} />
+      <>
+        <Grid
+          mt={2}
+          item
+          xs={12}
+          display={"flex"}
+          justifyContent={"space-between"}
+        >
+          <TabsWrapper
+            onChange={handleTabsChange}
+            value={currentTab}
+            variant="scrollable"
+            scrollButtons="auto"
+            textColor="primary"
+            indicatorColor="primary"
+          >
+            {tabs.map((tab) => (
+              <Tab
+                sx={{ fontSize: ".7rem" }}
+                key={tab.value}
+                label={tab.label}
+                value={tab.value}
+              />
+            ))}
+          </TabsWrapper>
         </Grid>
-      </Paper>
-
-      {/* Additional Data */}
-      <Paper elevation={3} style={paperStyle}>
-        <Typography variant="h5" gutterBottom>
-          Additional Data
-        </Typography>
-        <Divider />
-        <Grid container spacing={2}>
-          {/* Add fields from Additional Data */}
+        <Grid item xs={12}>
+          {currentTab === "projects" && <ClientDetailsOverview data={data} />}
+          {currentTab === "devs" && (
+            <div>Column for displaying deves with roles</div>
+          )}
+          {currentTab === "tasks" && <div>Column for displaying the task</div>}
         </Grid>
-      </Paper>
-
-      {/* Communication Type */}
-      <Paper elevation={3} style={paperStyle}>
-        <Typography variant="h5" gutterBottom>
-          Communication Type
-        </Typography>
-        <Divider />
-        <Grid container spacing={2}>
-          {/* Add fields from Communication Type */}
-        </Grid>
-      </Paper>
-    </Container>
+      </>
+    </Grid>
   );
 };
-
-const ClientDetails: React.FC = () => {
-  return (
-    <Container>
-      <Paper sx={{ padding: 4, maxWidth: 800, margin: "auto" }} elevation={3}>
-        <Typography variant="h4">Client Details</Typography>
-
-        <Box>
-          <Typography variant="h5">Developer Roles</Typography>
-          <Divider />
-          <DeveloperRole
-            role="Front-end Developer"
-            description="Responsible for the client's website's user interface."
-          />
-        </Box>
-
-        <Box>
-          <Typography variant="h5">About the Company</Typography>
-          <Divider />
-          <Typography>
-            Provides a detailed description of the client company, its mission,
-            values, and culture.
-          </Typography>
-        </Box>
-
-        <Box>
-          <Typography variant="h5">Company Logo</Typography>
-          <Divider />
-          <Avatar alt="Company Logo" src="company_logo_url" />
-        </Box>
-
-        <Box>
-          <Typography variant="h5">Team Members</Typography>
-          <Divider
-            sx={{
-              margin: 2,
-            }}
-          />
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={4}>
-              <TeamMember
-                name="John Doe"
-                role="CTO"
-                profile="team_member_profile_url"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={4}>
-              <TeamMember
-                name="Jane Smith"
-                role="Lead Developer"
-                profile="team_member_profile_url"
-              />
-            </Grid>
-            {/* Add more team members as needed */}
-          </Grid>
-        </Box>
-
-        {/* Add more sections as needed */}
-      </Paper>
-    </Container>
-  );
-};
-
 export default ClientDetails;
