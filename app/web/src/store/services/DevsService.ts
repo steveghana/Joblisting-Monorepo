c2cc06e33a4ef1725bba4417c9776958ce8f0db2
steveghana <boatengstephen707@gmail.com>
2023-11-30 22:49:05 +0000

    Batch Committing file: app/web/src/store/services/DevsService.ts

--- app/web/src/store/services/DevsService.ts
+++ app/web/src/store/services/DevsService.ts
@@ -3,53 +3,54 @@ import { IProfession } from "../../types/roles";
 import axios from "axios";
 import _api_url from "../../api/_api_url";
 import { IDev } from "../../types/devs";
+import { IClient } from "../../types/client";
 export const DEV_API_KEY = "DevApi";
 export const devApi = createApi({
   reducerPath: DEV_API_KEY,
   baseQuery: fetchBaseQuery({ baseUrl: _api_url.getApiUrl() }), // Replace with your actual API URL
   endpoints: (builder) => ({
-    updateDev: builder.mutation<IDev, { id: string; data: IDev }>({
-      query: ({ data, id }) => ({
-        url: `dev/${id}`, // Replace with the appropriate API endpoint
-        method: "UPDATE",
-        body: data,
+    updateDev: builder.mutation<IDev, { id: string; data: Partial<IDev> }>({
+      query: (body) => ({
+        url: `developers/${body.id}`, // Replace with the appropriate API endpoint
+        method: "PATCH",
+        body,
       }),
     }),
     deletDev: builder.mutation<IDev, { id: string }>({
       query: ({ id }) => ({
-        url: `dev/delete${id}`, // Replace with the appropriate API endpoint
+        url: `developers/${id}`, // Replace with the appropriate API endpoint
         method: "DELETE",
       }),
     }),
 
     addDev: builder.mutation<IDev, IDev>({
       query: (dev) => ({
-        url: "Dev/add", // Replace with the appropriate API endpoint
+        url: "developers", // Replace with the appropriate API endpoint
         method: "POST",
         body: dev,
       }),
     }),
-    getDevs: builder.query<string, void>({
+    getDevs: builder.query<IDev[], void>({
       query: () => ({
         url: "developers", // Replace with the appropriate API endpoint
-        // method: "GET",
-        responseHandler: (response: { text: () => any }) => response.text(),
+        method: "GET",
       }),
-      transformResponse: (response, meta) => {
-        const parsedData = JSON.parse(response as string);
-        console.log(parsedData);
-        return parsedData;
+      transformResponse: (response: IDev[], meta) => {
+        return response;
       },
       // Pick out errors and prevent nested properties in a hook or selector
-      transformErrorResponse: (
-        response: { status: string | number },
-        meta,
-        arg
-      ) => response.status,
+      transformErrorResponse: (response: any, meta, arg) => {
+        const {
+          data: {
+            error: { message },
+          },
+        } = response;
+        return Array.isArray(message) ? message.join(",") : message;
+      },
     }),
     getDev: builder.query<IDev, { id: string }>({
       query: ({ id }) => ({
-        url: `dev/get/${id}`, // Replace with the appropriate API endpoint
+        url: `developers/${id}`, // Replace with the appropriate API endpoint
         method: "GET",
       }),
     }),
