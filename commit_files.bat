2ec7aab740387fbed737c594b4e3726a960a23b2
steveghana <boatengstephen707@gmail.com>
2023-11-30 22:49:18 +0000

    Batch Committing file: commit_files.bat

--- commit_files.bat
+++ commit_files.bat
@@ -1,8 +1,7 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM Iterate through all files in the directory and its subdirectories
-for /r %%i in (*) do (
+for /f "tokens=*" %%i in ('git ls-files') do (
     git add "%%i"
     git commit -m "Batch Committing file: %%i"
 )
