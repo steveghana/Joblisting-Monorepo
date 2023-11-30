8037f90bdb6b174c73d8e30baf1754b9a2706e2c
steveghana <boatengstephen707@gmail.com>
2023-11-30 22:48:29 +0000

    Batch Committing file: app/api/src/types/developer.ts

--- app/api/src/types/developer.ts
+++ app/api/src/types/developer.ts
@@ -5,6 +5,7 @@ import { Iinterviews } from './interviews';
 import { IRole } from './role';
 import { IUser } from './user';
 import { Interview } from '@/apps/interviews/entities/interview.entity';
+import { IClient } from './client';
 
 export interface IDev {
   id?: string;
@@ -16,14 +17,14 @@ export interface IDev {
 
   skills: string[];
   phone_number: string;
-
+  salary: number;
   address: string;
 
   role_status: 'InHouse' | 'Pending' | 'Accepted' | 'External';
   // application: IApplication;
   // In House, Pending Interview, External
   years_of_experience: string;
-
+  client: IClient;
   roles: IRole;
   // Define the associations with Interviews, ClockHours, and Applications
   interviewsAsInterviewer?: Partial<Interview>[];
