88468271a609916f53e980137f68fab49d060c6b
steveghana <boatengstephen707@gmail.com>
2023-11-30 23:03:58 +0000

    Automated Commit file: _update commit_files.bat

--- commit_files.bat
+++ commit_files.bat
@@ -2,8 +2,11 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 for /f "tokens=*" %%i in ('git ls-files') do (
+    set "file=%%i"
+    set "filename=!file:%cd%\=!"
+    
     git add "%%i"
-    git commit -m "Batch Committing file: %%i"
+    git commit -m "Automated Commit file: _update !filename!"
 )
 
 git push
