c95762d882bb179b44ca6116cc00ab90a92dc809
steveghana <boatengstephen707@gmail.com>
2023-12-01 21:41:47 +0000

    Automated Commit file: _update app/web/src/types/client.ts

--- app/web/src/types/client.ts
+++ app/web/src/types/client.ts
@@ -1,4 +1,5 @@
 import { CountryType } from "../components/settings/CountrySelector";
+import { IDev } from "./devs";
 import { IRoleData } from "./roles";
 export interface IClient {
   id?: string;
@@ -7,6 +8,9 @@ export interface IClient {
   email: string;
   industry: string[];
   numOfEmployees: string;
+  developersLength: number;
+  rolesOpen: number;
+  countrylabel: string;
   companyName: string;
   phoneNumber: string;
   projectTitle: string;
