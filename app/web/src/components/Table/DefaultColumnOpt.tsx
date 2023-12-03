34e0c2956457deaf9b1d3e5b140551952a85f58b
steveghana <boatengstephen707@gmail.com>
2023-12-03 03:05:55 +0000

    Automated Commit file: _update app/web/src/components/Table/DefaultColumnOpt.tsx

--- app/web/src/components/Table/DefaultColumnOpt.tsx
+++ app/web/src/components/Table/DefaultColumnOpt.tsx
@@ -1,9 +1,9 @@
 import { type MRT_RowData, type MRT_TableOptions } from "material-react-table";
 
 //define re-useable default table options for all tables in your app
-export const getDefaultMRTOptions = <TData extends MRT_RowData>(): Partial<
-  MRT_TableOptions<TData>
-> => ({
+export const getDefaultMRTOptions = <TData extends MRT_RowData>(
+  isMobile: boolean
+): Partial<MRT_TableOptions<TData>> => ({
   //list all of your default table options here
   enableColumnFilterModes: true,
   createDisplayMode: "modal", //default ('row', and 'custom' are also available)
@@ -16,8 +16,11 @@ export const getDefaultMRTOptions = <TData extends MRT_RowData>(): Partial<
   enableRowActions: true,
   // showAlertBanner:false,
   enableRowSelection: true,
-  // enableRowSelection: (row: MRT_Row) => console.log('ite w'),
-  initialState: { showColumnFilters: true, showGlobalFilter: true },
+  // // enableRowSelection: (row: MRT_Row) => console.log('ite w'),
+  initialState: {
+    showColumnFilters: true,
+    showGlobalFilter: isMobile ? true : false,
+  },
   paginationDisplayMode: "pages",
   positionToolbarAlertBanner: "bottom",
   muiSearchTextFieldProps: {
