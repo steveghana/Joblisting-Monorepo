11732f05276c869879e77d7090dd9fe312abef4c
steveghana <boatengstephen707@gmail.com>
2023-12-03 03:05:56 +0000

    Automated Commit file: _update app/web/src/components/Table/TableActions.tsx

--- app/web/src/components/Table/TableActions.tsx
+++ app/web/src/components/Table/TableActions.tsx
@@ -1,37 +1,60 @@
-import React from "react";
-import { IDev } from "../../types/devs";
 import { MRT_Row, MRT_TableInstance } from "material-react-table";
 import { Box, IconButton, Tooltip } from "@mui/material";
 import { Delete, Edit, People } from "@mui/icons-material";
+import CustomButton from "../button";
 interface IActions<T> {
   row: MRT_Row<T>;
   table: MRT_TableInstance<T>;
   onConfirmDelete: () => void;
+  isDetails?: boolean;
+  handleOpenJobForm?: (id: string) => void;
 }
-function TableActions<T>({ row, table, onConfirmDelete }: IActions<T>) {
+function TableActions<T>({
+  row,
+  table,
+  onConfirmDelete,
+  handleOpenJobForm,
+  isDetails,
+}: IActions<T>) {
   return (
-    <Box sx={{ display: "flex", gap: ".5rem" }}>
-      <Tooltip title="Edit">
-        <IconButton
-          onClick={(e) => {
-            e.stopPropagation();
-            table.setEditingRow(row);
-          }}
-        >
-          <Edit />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="Delete">
-        <IconButton
-          color="error"
-          onClick={(e) => {
-            e.stopPropagation();
-            onConfirmDelete();
-          }}
-        >
-          <Delete />
-        </IconButton>
-      </Tooltip>
+    <Box sx={{ display: "flex", gap: ".2rem" }}>
+      {isDetails && (
+        <Tooltip title="add new job">
+          <IconButton
+            onClick={(e) => {
+              e.stopPropagation();
+              handleOpenJobForm(row.id);
+            }}
+          >
+            <CustomButton sx={{ my: 2 }} variant="contained" text="Add Job" />
+          </IconButton>
+        </Tooltip>
+      )}
+      <Box display={"flex"} justifyContent={"space-between"}>
+        <Tooltip title="Edit">
+          <IconButton
+            // sx={{ minWidth: "50px" }}
+            onClick={(e) => {
+              e.stopPropagation();
+              table.setEditingRow(row);
+            }}
+          >
+            <Edit />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton
+            // sx={{ minWidth: "50px" }}
+            color="error"
+            onClick={(e) => {
+              e.stopPropagation();
+              onConfirmDelete();
+            }}
+          >
+            <Delete />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
   );
 }
