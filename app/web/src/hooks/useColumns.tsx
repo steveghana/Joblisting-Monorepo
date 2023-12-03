265f3fc96eea010b42e84ba1868a90a5a9413664
steveghana <boatengstephen707@gmail.com>
2023-12-03 03:06:11 +0000

    Automated Commit file: _update app/web/src/hooks/useColumns.tsx

--- app/web/src/hooks/useColumns.tsx
+++ app/web/src/hooks/useColumns.tsx
@@ -4,6 +4,8 @@ import { IDev } from "../types/devs";
 import { Avatar, Box } from "@mui/material";
 import { AccountCircle, Send } from "@mui/icons-material";
 import { IClient } from "../types/client";
+import { validateEmail, validateRequired } from "../utils/tablevalidate";
+import { IRoleData } from "../types/roles";
 export const useClientColums = () => {
   return useMemo<MRT_ColumnDef<IClient>[]>(
     () => [
@@ -245,7 +247,111 @@ export const useDevsColums = () => {
     []
   );
 };
+export const useClientRolesColumn = () => {
+  return useMemo<MRT_ColumnDef<IRoleData>[]>(
+    () => [
+      {
+        id: "employee", //id used to define `group` column
+        header: "Role Info",
+        columns: [
+          {
+            accessorFn: (row) => `${row.title}`, //accessorFn used to join multiple data into a single cell
+            id: "name", //id is still required when using accessorFn instead of accessorKey
+            header: "Title",
+            enableEditing: true,
 
+            size: 50,
+          },
+          {
+            accessorKey: "communicationPreferences", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+            enableClickToCopy: true,
+            enableEditing: true,
+
+            filterVariant: "autocomplete",
+            header: "Comm. Type",
+            size: 50,
+          },
+          {
+            id: "methodology",
+            accessorKey: "methodology", //hey a simple column for once
+            header: "Methodology",
+            enableEditing: true,
+            size: 50,
+          },
+          // {
+          //   id: "whentostart",
+          //   accessorKey: "whenToStart", //hey a simple column for once
+          //   header: "When To Start",
+          //   enableEditing: true,
+          //   size: 200,
+          // },
+          {
+            accessorKey: "experience", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+            enableClickToCopy: true,
+            filterVariant: "autocomplete",
+            enableEditing: true,
+
+            header: "Exp Required",
+            size: 50,
+          },
+        ],
+      },
+      {
+        id: "id",
+        header: "Job Info",
+        columns: [
+          {
+            accessorKey: "vacancy_status",
+            // filterVariant: 'range', //if not using filter modes feature, use this instead of filterFn
+            filterFn: "between",
+            header: "Vacancy Status",
+            enableEditing: true,
+
+            size: 50,
+            //custom conditional format and styling
+            Cell: ({ cell }) => (
+              <Box
+                component="span"
+                sx={(theme) => ({
+                  backgroundColor:
+                    cell.getValue<string>() === "Closed"
+                      ? theme.palette.info.dark
+                      : theme.palette.success.dark,
+
+                  borderRadius: "0.25rem",
+                  color: "#fff",
+                  maxWidth: "9ch",
+                  p: "0.25rem",
+                })}
+              >
+                {cell.getValue<string>()}
+              </Box>
+            ),
+          },
+
+          //  {
+          //    accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
+          //    id: "startDate",
+          //    header: "Start Date",
+          //    enableEditing: false,
+
+          //    filterVariant: "date",
+          //    filterFn: "lessThan",
+          //    sortingFn: "datetime",
+          //    Cell: ({ cell }) => cell.getValue<Date>()?.toLocaleDateString(), //render Date as a string
+          //    Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+          //    muiFilterTextFieldProps: {
+          //      sx: {
+          //        minWidth: "250px",
+          //      },
+          //    },
+          //  },
+        ],
+      },
+    ],
+    []
+  );
+};
 type ClientColumnsType =
   MRT_ColumnDef<IClient>[] /* specify the type returned by useClientColums */;
 type DevColumnsType =
