61449fe2067d97ce94081d2e219ca9f76dd76fff
steveghana <boatengstephen707@gmail.com>
2023-12-07 18:44:46 +0000

    Automated Commit file: _update app/api/src/apps/interviews/dto/create-interview.dto.ts

--- app/api/src/apps/interviews/dto/create-interview.dto.ts
+++ app/api/src/apps/interviews/dto/create-interview.dto.ts
@@ -3,31 +3,24 @@ import { Iinterviews } from '@/types/interviews';
 import { IRole } from '@/types/role';
 import { IsNotEmpty } from 'class-validator';
 type IinterviewAs = 'interviewee' | 'interviewer';
-type Interview = Iinterviews & {
-  interviewAs: IinterviewAs;
-  interveiweeId: string;
-  interviewerId: string;
-};
-export class CreateInterviewDto implements Interview {
-  interviewAs: IinterviewAs;
-  @IsNotEmpty({
-    message: 'please enter your address',
-  })
-  roleId: string;
+
+export class CreateInterviewDto {
   @IsNotEmpty({
     message: 'please enter your scheduled date',
   })
   scheduled_date: Date;
+  @IsNotEmpty({
+    message: 'please enter interview status',
+  })
   status: 'Scheduled' | 'Completed' | 'Canceled';
   @IsNotEmpty({
     message: 'interviewee Id is required',
   })
-  interveiweeId: string;
+  candidateId: string;
   @IsNotEmpty({
     message: 'interviewer Id is required',
   })
   interviewerId: string;
-  interviewee: IDev;
-  interviewer: IDev;
-  role: IRole;
+
+  // role: IRole;
 }
