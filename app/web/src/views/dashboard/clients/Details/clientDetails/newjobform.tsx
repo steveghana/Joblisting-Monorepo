b83fe0a90b043f191cb3fedec684f3adfec2c5f5
steveghana <boatengstephen707@gmail.com>
2023-12-08 23:13:49 +0000

    Automated Commit file: _update app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx

--- app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx
+++ app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx
@@ -1,14 +1,6 @@
 // NewJobForm.tsx
 import React from "react";
-import { Formik, Form, Field } from "formik";
-import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  TextField,
-  DialogActions,
-} from "@mui/material";
+import { Dialog, DialogTitle, DialogContent } from "@mui/material";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import RoleInfo from "../clientForm/addtionallInfo";
 
@@ -18,25 +10,23 @@ interface NewJobFormProps {
 }
 
 const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose }) => {
-  const initialValues = {
-    description: "",
-    roleType: "Remote",
-    whenToStart: "",
-    employmentType: "",
-    country: "",
-    selectedSkills: [],
-    roleName: "",
-    jobType: "",
-    salary: "",
-    tasks: [],
-    roleCategory: "",
-  };
+  // const initialValues = {
+  //   description: "",
+  //   roleType: "Remote",
+  //   whenToStart: "",
+  //   employmentType: "",
+  //   country: "",
+  //   selectedSkills: [],
+  //   roleName: "",
+  //   jobType: "",
+  //   salary: "",
+  //   tasks: [],
+  //   roleCategory: "",
+  // };
 
   const handleSubmit = (values: any, { resetForm }: any) => {
-    // Implement logic to create a new job here
     console.log(values);
 
-    // Reset the form after submission
     resetForm();
     onClose();
   };
