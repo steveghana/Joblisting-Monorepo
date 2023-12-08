2dc7b012832a3abe76021cc195afa16727ad3ee8
steveghana <boatengstephen707@gmail.com>
2023-12-08 23:13:01 +0000

    Automated Commit file: _update app/api/src/util/uuid.ts

--- app/api/src/util/uuid.ts
+++ app/api/src/util/uuid.ts
@@ -2,7 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 // v4 from uuid can't be stubbed, so we use a delegating function that can be
 function makeUuid(): string {
-  console.log(uuidv4(), 'uuid ');
   return uuidv4();
 }
 
