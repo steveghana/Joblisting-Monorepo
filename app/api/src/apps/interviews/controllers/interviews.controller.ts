eaacb512eab0d7326df07c01d1e9552e7b405b78
steveghana <boatengstephen707@gmail.com>
2023-12-08 23:12:52 +0000

    Automated Commit file: _update app/api/src/apps/interviews/controllers/interviews.controller.ts

--- app/api/src/apps/interviews/controllers/interviews.controller.ts
+++ app/api/src/apps/interviews/controllers/interviews.controller.ts
@@ -6,40 +6,85 @@ import {
   Patch,
   Param,
   Delete,
+  UseFilters,
 } from '@nestjs/common';
 import { InterviewsService } from '../services/interviews.service';
 import { CreateInterviewDto } from '../dto/create-interview.dto';
 import { UpdateInterviewDto } from '../dto/update-interview.dto';
-
+import {
+  ApiBadRequestResponse,
+  ApiInternalServerErrorResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { HttpExceptionFilter } from '../../../middleware/err.Middleware';
 @Controller('interviews')
 export class InterviewsController {
   constructor(private readonly interviewsService: InterviewsService) {}
 
   @Post()
-  create(@Body() createInterviewDto: CreateInterviewDto) {
-    console.log(createInterviewDto);
-    return this.interviewsService.create(createInterviewDto);
+  @ApiTags('creat interview')
+  @ApiOperation({
+    description: 'create a new interview',
+  })
+  @UseFilters(new HttpExceptionFilter())
+  @ApiBadRequestResponse({ description: 'Bad Request something went wrong' })
+  @ApiInternalServerErrorResponse({ description: 'Server is down' })
+  public async create(@Body() createInterviewDto: CreateInterviewDto) {
+    const result = await this.interviewsService.create(createInterviewDto);
+    return result;
   }
 
   @Get()
-  findAll() {
-    return this.interviewsService.findAll();
+  @ApiTags('get interviews')
+  @ApiOperation({
+    description: 'Get all interviews',
+  })
+  @UseFilters(new HttpExceptionFilter())
+  @ApiBadRequestResponse({ description: 'Bad Request something went wrong' })
+  @ApiInternalServerErrorResponse({ description: 'Server is down' })
+  public async findAll() {
+    const result = await this.interviewsService.findAll();
+    return result;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.interviewsService.findOne(id);
+  @ApiTags('get interview')
+  @ApiOperation({
+    description: 'Get an interview',
+  })
+  @UseFilters(new HttpExceptionFilter())
+  @ApiBadRequestResponse({ description: 'Bad Request something went wrong' })
+  @ApiInternalServerErrorResponse({ description: 'Server is down' })
+  public async findOne(@Param('id') id: string) {
+    const result = await this.interviewsService.findOne(id);
+    return result;
   }
 
   @Patch(':id')
-  update(
+  @ApiTags('Update interview')
+  @ApiOperation({
+    description: 'Update an interview',
+  })
+  @UseFilters(new HttpExceptionFilter())
+  @ApiBadRequestResponse({ description: 'Bad Request something went wrong' })
+  @ApiInternalServerErrorResponse({ description: 'Server is down' })
+  public async update(
     @Param('id') id: string,
     @Body() updateInterviewDto: UpdateInterviewDto,
   ) {
-    return this.interviewsService.update(id, updateInterviewDto);
+    const result = await this.interviewsService.update(id, updateInterviewDto);
+    return result;
   }
 
   @Delete(':id')
+  @ApiTags('Delete interview')
+  @ApiOperation({
+    description: 'Delete an interview',
+  })
+  @UseFilters(new HttpExceptionFilter())
+  @ApiBadRequestResponse({ description: 'Bad Request something went wrong' })
+  @ApiInternalServerErrorResponse({ description: 'Server is down' })
   remove(@Param('id') id: string) {
     return this.interviewsService.cancel(id);
   }
