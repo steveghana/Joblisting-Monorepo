49df6be991a8e190a689ddc8137842c836af8dbe
steveghana <boatengstephen707@gmail.com>
2023-12-08 02:17:07 +0000

    Automated Commit file: _update app/api/src/apps/interviews/services/interviews.service.ts

--- app/api/src/apps/interviews/services/interviews.service.ts
+++ app/api/src/apps/interviews/services/interviews.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
 import { CreateInterviewDto } from '../dto/create-interview.dto';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+
 import { UpdateInterviewDto } from '../dto/update-interview.dto';
 import Interviews from '../dataManager';
 import { useTransaction } from '../../../util/transaction';
+import Developers from '../../../apps/developers/dataManager';
 
 @Injectable()
 export class InterviewsService {
   create(createInterviewDto: CreateInterviewDto) {
     // createInterviewDto.
     return useTransaction(async (transaction) => {
-      return Interviews.createInterviews(createInterviewDto, transaction);
+      const interviewResponse = await Interviews.createInterviews(
+        createInterviewDto,
+        transaction,
+      );
+      const devId = interviewResponse.candidate.id;
+      await Developers.update(
+        devId,
+
+        { role_status: 'Interviewing' },
+        transaction,
+      );
+      return interviewResponse;
     });
   }
 
@@ -17,15 +30,37 @@ export class InterviewsService {
     return `This action returns all interviews`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} interview`;
   }
 
-  update(id: number, updateInterviewDto: UpdateInterviewDto) {
+  update(id: string, updateInterviewDto: UpdateInterviewDto) {
     return `This action updates a #${id} interview`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} interview`;
+  cancel(interviewId: string) {
+    return useTransaction(async (transaction) => {
+      const {
+        candidate: { id },
+      } = await Interviews.getById(interviewId);
+      const updateDev = await Developers.update(
+        id,
+        { role_status: 'Pending' },
+        transaction,
+      );
+
+      const canceldInterview = await Interviews.cancleInterview(
+        id,
+        transaction,
+      );
+      if (!canceldInterview) {
+        throw new HttpException(
+          'Couldnt cancel interview, please try again later',
+          HttpStatus.BAD_REQUEST,
+        );
+        //NOtify the respective candidates the interview has been canceled
+      }
+      return canceldInterview;
+    });
   }
 }
