8b47c5ed40269dddfa134364db98566d689f3354
steveghana <boatengstephen707@gmail.com>
2023-12-09 23:13:49 +0000

    Automated Commit file: _update app/web/src/views/HR/index.tsx

--- app/web/src/views/HR/index.tsx
+++ app/web/src/views/HR/index.tsx
@@ -4,28 +4,24 @@ import NoData from "../../components/NoData";
 import { useDevsShortlistedColums } from "../../hooks/useShortlistedDevsColumns";
 import { useGetDevsQuery } from "../../store/services/dev.service";
 import DevTableData from "../Devs/DevColumns";
-import InterviewPage from "./interview";
 import { useNavigate } from "react-router";
+import { useTypedDispatch, useTypedSelector } from "../../store";
+import { fetchDevs } from "../../store/slices/dev.slice";
 
 const Shortlisted = () => {
-  const {
-    data: devs,
-    isLoading,
-    isFetching,
-    isError,
-    refetch,
-  } = useGetDevsQuery();
+  const { devs, error, isError, isFetching, isloading } = useTypedSelector(
+    (state) => state.devs
+  );
+  // State is used as Cache from devs api cannot be invalidated from the interview page
   const [openRoleForm, setOpenRoleForm] = React.useState(false);
-  const [applicantId, setApplicantID] = useState<string>();
-  const handleOpenInterviewForm = (id: string) => {
-    setOpenRoleForm(true);
-  };
+
   const handleCloseJobForm = () => {
     setOpenRoleForm(false);
   };
   // const hasInterview = devs.
   const columns = useDevsShortlistedColums();
   const navigate = useNavigate();
+  const dispatch = useTypedDispatch();
   const devsShortlistedData =
     (devs?.length &&
       devs.filter(
@@ -33,28 +29,25 @@ const Shortlisted = () => {
           rolestatus === "Pending" || rolestatus === "Interviewing"
       )) ||
     [];
-  console.log(devs);
-  if (isLoading || isFetching) {
+  if (isloading || isFetching) {
     return <FullscreenProgress />;
   }
   if (!devsShortlistedData.length) {
     return <NoData />;
   }
   return (
-    <>
-      <InterviewPage onClose={handleCloseJobForm} open={openRoleForm} />
-
-      <DevTableData
-        handleOpenInterviewForm={(id) => navigate(`/hr/interviews/${id}`)}
-        tableType="Shortlist"
-        columns={columns}
-        devs={devsShortlistedData}
-        refetch={() => refetch()}
-        isLoading={isLoading}
-        isError={isError}
-        isFetching={isFetching}
-      />
-    </>
+    <DevTableData
+      handleOpenInterviewForm={(id) => navigate(`/hr/interviews/${id}`)}
+      tableType="Shortlist"
+      columns={columns}
+      devs={devsShortlistedData}
+      refetch={
+        () => dispatch(fetchDevs()) // update the persisted state
+      }
+      isLoading={isloading}
+      isError={isError}
+      isFetching={isFetching}
+    />
   );
 };
 export default Shortlisted;
