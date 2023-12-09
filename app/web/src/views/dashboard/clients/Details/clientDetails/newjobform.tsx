2539fbf16b3490b684a2b339ac050312ac808a60
steveghana <boatengstephen707@gmail.com>
2023-12-09 07:06:58 +0000

    Automated Commit file: _update app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx

--- app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx
+++ app/web/src/views/dashboard/clients/Details/clientDetails/newjobform.tsx
@@ -3,31 +3,35 @@ import React from "react";
 import { Dialog, DialogTitle, DialogContent } from "@mui/material";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import RoleInfo from "../clientForm/addtionallInfo";
+import { useAddJobMutation } from "../../../../../store/services/role.service";
+import { toast } from "react-toastify";
 
 interface NewJobFormProps {
   open: boolean;
   onClose: () => void;
+  roleId: string;
 }
 
-const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose }) => {
-  // const initialValues = {
-  //   description: "",
-  //   roleType: "Remote",
-  //   whenToStart: "",
-  //   employmentType: "",
-  //   country: "",
-  //   selectedSkills: [],
-  //   roleName: "",
-  //   jobType: "",
-  //   salary: "",
-  //   tasks: [],
-  //   roleCategory: "",
-  // };
+const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose, roleId }) => {
+  const [createJob, { isLoading }] = useAddJobMutation();
 
-  const handleSubmit = (values: any, { resetForm }: any) => {
+  const handleSubmit = async (values: any) => {
     console.log(values);
+    try {
+      const response = await createJob({ ...values, roleId }).unwrap();
+      if (!isLoading && response) {
+        toast.success("Job Added Successfully", {
+          position: "bottom-center",
+        });
+        onClose();
+      }
+    } catch (error) {
+      toast.error("Error submitting data", {
+        position: "bottom-center",
+      });
+      return error;
+    }
 
-    resetForm();
     onClose();
   };
 
@@ -45,7 +49,7 @@ const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose }) => {
         <DialogContent>
           <RoleInfo
             atClientPage={true}
-            handleExternalSubmit={(values) => console.log(values)}
+            handleExternalSubmit={(values) => handleSubmit(values)}
           />
         </DialogContent>
       </PerfectScrollbar>
