2445aac284ad3beb5b7a09402f6e29dc0f1e2a21
steveghana <boatengstephen707@gmail.com>
2023-12-11 17:56:13 +0000

    Automated Commit file: _update app/api/src/apps/interviews/dto/create-interview.dto.ts

--- app/api/src/apps/interviews/dto/create-interview.dto.ts
+++ app/api/src/apps/interviews/dto/create-interview.dto.ts
@@ -2,13 +2,9 @@ import { IDev } from '@/types/developer';
 import { Iinterviews } from '@/types/interviews';
 import { IRole } from '@/types/role';
 import { IsNotEmpty } from 'class-validator';
-type IinterviewAs = 'interviewee' | 'interviewer';
+type IinterviewAs = 'interviewee' | 'guest';
 
 export class CreateInterviewDto {
-  @IsNotEmpty({
-    message: 'please enter your scheduled date',
-  })
-  scheduled_date: Date;
   @IsNotEmpty({
     message: 'please enter interview status',
   })
@@ -18,9 +14,41 @@ export class CreateInterviewDto {
   })
   candidateId: string;
   @IsNotEmpty({
-    message: 'interviewer Id is required',
+    message: 'guest Id is required',
   })
-  interviewerId: string;
+  guests: string[];
 
+  @IsNotEmpty({
+    message: 'guest Id is required',
+  })
+  eventType: string;
+  @IsNotEmpty({
+    message: 'guest Id is required',
+  })
+  eventOption: string;
+  @IsNotEmpty({
+    message: 'desicrpit Id is required',
+  })
+  description: string;
+  @IsNotEmpty({
+    message: 'even link Id is required',
+  })
+  eventLInk: string;
+  @IsNotEmpty({
+    message: 'start time is required',
+  })
+  starttime: string;
+  @IsNotEmpty({
+    message: 'end time is required',
+  })
+  endtime: string;
+  @IsNotEmpty({
+    message: 'start date is required',
+  })
+  startDate: Date;
+  @IsNotEmpty({
+    message: 'end date is required',
+  })
+  endDate: Date;
   // role: IRole;
 }
