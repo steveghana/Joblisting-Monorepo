e7d9ae0f889f8bcf15bff8479e5e1b5e02737eae
steveghana <boatengstephen707@gmail.com>
2023-12-11 06:34:34 +0000

    Automated Commit file: _update app/web/src/views/Devs/Interview/interviewEdit.tsx

--- app/web/src/views/Devs/Interview/interviewEdit.tsx
+++ app/web/src/views/Devs/Interview/interviewEdit.tsx
@@ -5,88 +5,140 @@ import {
   DialogContent,
   DialogTitle,
   Paper,
+  Typography,
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import CustomButton from "../../../components/button";
-import SelectParticipants, {
-  InterviewFormValue,
-} from "../../HR/interviewSteps/SelectParticipants";
-import { Iinterviews } from "../../../types/interviews";
+// import SelectParticipants, {
+//   InterviewFormValue,
+// } from "../../HR/interviewSteps/SelectParticipants";
+import { Iinterviews, InterviewFormValue } from "../../../types/interviews";
 import { IDev } from "../../../types/devs";
-type IEdit = {
-  editDialogOpen: boolean;
-  handleEditClose: () => void;
-  interviewEditData: Iinterviews;
-  devsAndApplicants: IDev[];
-};
-const InterviewEdit = ({
-  editDialogOpen,
-  handleEditClose,
-  devsAndApplicants,
-  interviewEditData,
-}: IEdit) => {
+import { useNavigate, useParams } from "react-router";
+import { useGetInterviewQuery } from "../../../store/services/interview.service";
+import { useTypedSelector } from "../../../store";
+import { toast } from "react-toastify";
+import Demo from "../../HR/Events/demo";
+import NoData from "../../../components/NoData";
+import FullscreenProgress from "../../../components/FullscreenProgress/FullscreenProgress";
+import MainCard from "../../../components/MainCard";
+
+const InterviewEdit = () => {
   const theme = useTheme();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const {
+    data: interviewEditData,
+    isFetching,
+    isError,
+    isLoading,
+    refetch,
+  } = useGetInterviewQuery({ id });
+
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+  const [eventName, setEventName] = useState("");
+  const [eventDescription, setEventDescription] = useState("");
   const matchUpMd = useMediaQuery(theme.breakpoints.up("md"));
-  const __applicant =
-    !!devsAndApplicants.length &&
-    devsAndApplicants.filter(
-      (applicant) =>
-        applicant.id !== interviewEditData.candidate.id &&
-        applicant.rolestatus === "Pending"
-    );
-  const interviewers =
-    !!devsAndApplicants?.length &&
-    devsAndApplicants.filter(
-      (interviewer) =>
-        interviewer.id !== interviewEditData.interviewer.id &&
-        interviewer.rolestatus === "Accepted"
-    );
+  const devsAndApplicants = useTypedSelector((state) => state.devs.devs);
+  if (isFetching || isLoading) {
+    return <FullscreenProgress />;
+  }
+  if (!interviewEditData || !devsAndApplicants) {
+    return <NoData />;
+  }
+  const __applicant = devsAndApplicants.filter(
+    (applicant) =>
+      applicant.id !== interviewEditData?.candidate.id &&
+      applicant.rolestatus === "Pending"
+  );
+  const interviewers = devsAndApplicants.filter(
+    (interviewer) =>
+      interviewer.id !== interviewEditData?.interviewer.id &&
+      interviewer?.rolestatus === "Accepted"
+  );
+
+  console.log(interviewEditData.candidate, "data");
+  // const session = useSession();
+  // const supabase = useSupabaseClient();
+  // const { isLoading } = useSessionContext();
+
+  // const handleGoogleSignIn = async () => {
+  //   const { error } = await supabase.auth.signInWithOAuth({
+  //     provider: "google",
+  //     options: {
+  //       scopes: "https://www.googleapis.com/auth/calendar",
+  //     },
+  //   });
+  //   if (error) {
+  //     console.error(
+  //       "Error logging in to Google provider with Supabase:",
+  //       error
+  //     );
+  //     toast.error("Failed to sign in with Google.");
+  //   }
+  // };
+
+  const handleSignOut = async () => {
+    // await supabase.auth.signOut();
+  };
+
+  const createCalendarEvent = async () => {
+    console.log("Creating calendar event");
+    {
+      const event = {
+        summary: eventName,
+        description: eventDescription,
+        start: {
+          dateTime: start.toISOString(),
+          timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        },
+        end: {
+          dateTime: end.toISOString(),
+          timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        },
+        attendees: [
+          { email: "john@example.com" },
+          { email: "jane@example.com" },
+          { email: "your-email@example.com" }, // You can include or exclude yourself
+        ],
+      };
+    }
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/calendar/v3/calendars/primary/events",
+        {
+          method: "POST",
+          headers: {
+            // Authorization: "Bearer " + session.provider_token,
+          },
+          body: JSON.stringify(event),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      toast.success("Event created, check your Google Calendar!");
+    } catch (error) {
+      console.error("Error creating calendar event:", error);
+      toast.error("Failed to create the event. Please try again.");
+    }
+  };
+  console.log(__applicant, "this is the applicant");
   const handleEdit = (value: InterviewFormValue) => {};
   return (
-    <Paper elevation={1}>
-      <Dialog open={editDialogOpen} onClose={handleEditClose}>
-        <DialogTitle>Edit Interview</DialogTitle>
-        <DialogContent>
-          <PerfectScrollbar
-            component="div"
-            style={{
-              height: !matchUpMd ? "calc(100vh - 56px)" : "calc(100vh - 88px)",
-              paddingLeft: "16px",
-              paddingRight: "16px",
-            }}
-          >
-            <SelectParticipants
-              _applicants={[interviewEditData.candidate, ...__applicant]}
-              interviewers={[interviewEditData.interviewer, ...interviewers]}
-              handleEdit={(values) => handleEdit(values)}
-              isEditing={true}
-              editableInterviewInfo={interviewEditData}
-            />
-          </PerfectScrollbar>
-          {/* Your form or input fields for editing */}
-        </DialogContent>
-        <DialogActions>
-          <CustomButton
-            variant="outlined"
-            fullWidth
-            color="primary"
-            type="submit"
-            text="Cancel"
-            onClick={handleEditClose}
-          />
-          <CustomButton
-            color="primary"
-            fullWidth
-            type="submit"
-            text="Save Changes"
-
-            /* onClick={handleSaveChanges} */
-          />
-        </DialogActions>
-      </Dialog>
-    </Paper>
+    <MainCard title={"Edit Interview"}>
+      <Demo
+        handleSubmit={(values) => console.log(values)}
+        _applicants={[interviewEditData?.candidate, ...__applicant]}
+        interviewers={[interviewEditData?.interviewer, ...interviewers]}
+        handleEdit={(values) => handleEdit(values)}
+        isEditing={true}
+        editableInterviewInfo={interviewEditData}
+      />
+      {/* Your form or input fields for editing */}
+    </MainCard>
   );
 };
 
