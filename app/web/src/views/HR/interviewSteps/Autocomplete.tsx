0068944fd73e56601487b488950e754aa0e5bb69
steveghana <boatengstephen707@gmail.com>
2023-12-11 06:34:35 +0000

    Automated Commit file: _update app/web/src/views/HR/interviewSteps/Autocomplete.tsx

--- app/web/src/views/HR/interviewSteps/Autocomplete.tsx
+++ app/web/src/views/HR/interviewSteps/Autocomplete.tsx
@@ -5,6 +5,7 @@ import { styled, lighten, darken } from "@mui/system";
 import {
   Avatar,
   Box,
+  Chip,
   CircularProgress,
   IconButton,
   InputProps,
@@ -13,6 +14,7 @@ import {
 import { useField } from "formik";
 import { ContactPageOutlined, Contacts } from "@mui/icons-material";
 import { IDev } from "../../../types/devs";
+import { useParams } from "react-router";
 
 const GroupHeader = styled("div")(({ theme }) => ({
   position: "sticky",
@@ -40,10 +42,22 @@ function highlightOption(text, inputValue) {
       regex.test(part) ? <b key={index}>{part}</b> : part
     );
 }
+// const userList = [
+//   { id: 1, name: "name 1" },
+//   { id: 2, name: "name 2" },
+//   { id: 3, name: "name 3" },
+//   { id: 4, name: "name 4" },
+//   { id: 5, name: "name 5" },
+// ];
 export default function RenderGroup({ label, data, ...props }: IGroup) {
+  const { id } = useParams();
+
   const [open, setOpen] = React.useState(false);
   const [field, meta, helpers] = useField(props.name);
-
+  const nameString = `${data[0]?.firstName || ""} ${data[0]?.lastName || ""}`;
+  const [value, setValue] = React.useState<string[] | []>(
+    id ? [nameString] : []
+  );
   //   const [options, setOptions] = React.useState<readonly Film[]>([]);
   const options = data.map((option) => {
     const firstLetter = option.firstName[0].toUpperCase();
@@ -56,86 +70,30 @@ export default function RenderGroup({ label, data, ...props }: IGroup) {
     helpers.setValue(value);
   };
   const loading = open && options.length === 0;
-
   return (
-    <Autocomplete
-      //   {...field}
-      //   {...props}
-      id="grouped-demo"
-      options={options.sort(
-        (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
-      )}
-      groupBy={(option) => option.firstLetter}
-      open={open}
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      getOptionLabel={(option) =>
-        `${option.firstName || ""} ${option.lastName || ""}` || ""
-      }
-      onChange={handleOnChange}
-      //   sx={{ width: 300 }}
-      isOptionEqualToValue={(option, value) =>
-        option.firstName === value.firstName
-      }
-      loading={loading}
-      renderInput={(params) => (
-        <TextField
-          fullWidth
-          {...params}
-          label={label}
-          InputProps={{
-            ...params.InputProps,
-            startAdornment: (
-              <React.Fragment>
-                {loading ? (
-                  <CircularProgress color="inherit" size={20} />
-                ) : null}
-                <IconButton
-                  onClick={() => {
-                    setOpen(!open);
-                  }}
-                  size="small"
-                >
-                  <Contacts />
-                </IconButton>
-              </React.Fragment>
-            ),
-          }}
-        />
-      )}
-      renderOption={(props, option, { inputValue }) => (
-        <li {...props}>
-          <Box display="flex" alignItems="center" gap={1}>
-            <Avatar
-              src={option.avatar || null}
-              sx={{ width: 30, height: 30 }}
+    <>
+      <Autocomplete
+        options={options.map((option) => option.firstName)}
+        onChange={handleOnChange}
+        // value={value}
+        defaultValue={value}
+        multiple
+        id="tags-filled"
+        loading={loading}
+        freeSolo
+        renderTags={(value: string[], getTagProps) =>
+          value.map((option: string, index: number) => (
+            <Chip
+              variant="outlined"
+              label={option}
+              {...getTagProps({ index })}
             />
-            <Typography>
-              {highlightOption(option.firstName, inputValue)}{" "}
-              {highlightOption(option.lastName, inputValue)}
-            </Typography>
-          </Box>
-        </li>
-      )}
-    />
+          ))
+        }
+        renderInput={(params) => (
+          <TextField fullWidth {...params} label={label} placeholder="Search" />
+        )}
+      />
+    </>
   );
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const topFilms = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "The Godfather", year: 1972 },
-  { title: "The Godfather: Part II", year: 1974 },
-  { title: "The Dark Knight", year: 2008 },
-  { title: "12 Angry Men", year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: "Pulp Fiction", year: 1994 },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-];
