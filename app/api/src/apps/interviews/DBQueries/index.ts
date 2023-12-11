15e12df2f3902db32a004f34a58b4a776cd34ae5
steveghana <boatengstephen707@gmail.com>
2023-12-11 23:58:22 +0000

    Automated Commit file: _update app/api/src/apps/interviews/DBQueries/index.ts

--- app/api/src/apps/interviews/DBQueries/index.ts
+++ app/api/src/apps/interviews/DBQueries/index.ts
@@ -3,7 +3,7 @@ import {
   injectDependencies,
 } from '../../../util/dependencyInjector';
 import uuidUtil from '../../../util/uuid';
-import { DeepPartial, EntityManager, In } from 'typeorm';
+import { DeepPartial, EntityManager, In, QueryRunner } from 'typeorm';
 import myDataSource from '../../../../db/data-source';
 import uuid from '../../../util/uuid';
 import { Iinterviews } from '@/types/interviews';
@@ -12,10 +12,62 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 // Import your actual interviews module
 
 export async function scheduleInterview(
-  interviewData: Omit<Iinterviews, 'guest' | 'interviewee' | 'role'> & {
-    candidateId: string;
-    guests: string[];
-  },
+  interviewData: Partial<
+    Omit<Iinterviews, 'guest' | 'interviewee' | 'role'> & {
+      candidateId: string;
+      guests: string[];
+    }
+  >,
+  transaction: EntityManager = null,
+  dependencies: Dependencies = null,
+) {
+  dependencies = injectDependencies(dependencies, ['db']);
+  const interviewRepo = transaction.getRepository(
+    dependencies.db.models.interviews,
+  );
+  const { guests: guestIds, candidateId, ...rest } = interviewData;
+  const devRepo = transaction.getRepository(dependencies.db.models.developer);
+
+  console.log('Candidate ID:', candidateId);
+  console.log('Guest IDs:', guestIds);
+
+  const candidate = await devRepo.findOne({
+    where: { id: candidateId },
+  });
+  const guests = await devRepo.find({
+    where: { id: In(guestIds) },
+  });
+
+  console.log('Candidate:', candidate);
+  console.log('Guests:', guests);
+
+  if (!candidate || guests.length !== interviewData.guests.length) {
+    throw new HttpException(
+      'Could not schedule interview. Check if Candidate or Developer(s) exist.',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+
+  const newInterview = interviewRepo.create({
+    candidate,
+    guests, // Assuming that the "guest" relationship is now an array
+    ...rest,
+    // ... other properties
+  });
+
+  let data = await interviewRepo.save(newInterview);
+
+  return data;
+}
+
+export async function updateInterview(
+  id: string,
+  interviewData: Partial<
+    Omit<Iinterviews, 'guest' | 'interviewee' | 'role'> & {
+      candidateId: string;
+      guests: string[];
+    }
+  >,
   transaction: EntityManager = null,
   dependencies: Dependencies = null,
 ) {
@@ -35,21 +87,30 @@ export async function scheduleInterview(
 
   if (!candidate || guests.length !== interviewData.guests.length) {
     throw new HttpException(
-      'Could not schedule interview. Check if Candidate or Developer(s) exist.',
+      'Could not update interview. Check if Candidate or Developer(s) exist.',
       HttpStatus.BAD_REQUEST,
     );
   }
 
-  const newInterview = interviewRepo.create({
-    candidate,
-    guests, // Assuming that the "guest" relationship is now an array
-    ...rest,
-    // ... other properties
+  // Load the existing interview
+  const existingInterview = await interviewRepo.findOne({
+    where: { id },
+    relations: ['guests'],
   });
 
-  let data = await interviewRepo.save(newInterview);
+  if (!existingInterview) {
+    throw new HttpException('Interview not found', HttpStatus.NOT_FOUND);
+  }
 
-  return data;
+  // Update interview properties
+  existingInterview.candidate = candidate;
+  existingInterview.guests = guests; // Update the many-to-many relationship
+  Object.assign(existingInterview, rest);
+
+  // Save the updated interview
+  await interviewRepo.save(existingInterview);
+
+  return 1; // Assuming 1 record was affected
 }
 
 export function getInterviewById(
