27bf967aff94be3bd99ff52db9e00aa105945f84
steveghana <boatengstephen707@gmail.com>
2023-12-13 23:23:37 +0000

    Automated Commit file: _update app/web/src/routes/index.ts

--- app/web/src/routes/index.ts
+++ app/web/src/routes/index.ts
@@ -1,11 +1,16 @@
 import { useRoutes } from "react-router-dom";
 
 // routes
-import MainRoutes, { homeRoutes } from "./MainRoutes";
+import MainRoutes, { ExternalRoutes, homeRoutes } from "./MainRoutes";
 import AuthenticationRoutes from "./AuthenticationRoutes";
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([homeRoutes, MainRoutes, AuthenticationRoutes]);
+  return useRoutes([
+    homeRoutes,
+    // ExternalRoutes,
+    MainRoutes,
+    AuthenticationRoutes,
+  ]);
 }
