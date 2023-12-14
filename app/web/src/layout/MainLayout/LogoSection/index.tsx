2571777a89d9d3497b3211d728cdae64f385b06b
steveghana <boatengstephen707@gmail.com>
2023-12-14 00:15:19 +0000

    Automated Commit file: _update app/web/src/layout/MainLayout/LogoSection/index.tsx

--- app/web/src/layout/MainLayout/LogoSection/index.tsx
+++ app/web/src/layout/MainLayout/LogoSection/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 // material-ui
-import { Box, ButtonBase } from "@mui/material";
+import { Box, ButtonBase, IconButton } from "@mui/material";
 import LogoImg from "../../../assets/images/Logo-Small-19.png";
 // project imports
 import config from "../../../config";
@@ -13,7 +13,21 @@ import { MENU_OPEN } from "../../../store/actions";
 const LogoSection = () => {
   const defaultId = useSelector((state: any) => state.customization?.defaultId);
   console.log(defaultId, "from log");
+  const lockSider = true;
   const dispatch = useDispatch();
+  if (lockSider) {
+    return (
+      <ButtonBase disableRipple>
+        <img
+          src={LogoImg}
+          style={{ objectFit: "contain" }}
+          alt="Logo"
+          width={100}
+          height={50}
+        />
+      </ButtonBase>
+    );
+  }
   return (
     <ButtonBase
       disableRipple
