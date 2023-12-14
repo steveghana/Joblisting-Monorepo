5faf4660edb48fe64f2caa7152653645cff442e5
steveghana <boatengstephen707@gmail.com>
2023-12-14 16:42:33 +0000

    Automated Commit file: _update app/web/src/main.tsx

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -5,14 +5,21 @@ import "../src/assets/scss/style.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import store, { persistor } from "./store";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { PersistGate } from "redux-persist/integration/react";
 import FullscreenProgress from "./components/FullscreenProgress/FullscreenProgress";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate /* s */ persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>
+  <GoogleOAuthProvider
+    clientId={
+      "98962994226-v6lsqu50ttq5jgtnu2qs8eucishhej5n.apps.googleusercontent.com"
+    }
+  >
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate /* s */ persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </GoogleOAuthProvider>
 );
