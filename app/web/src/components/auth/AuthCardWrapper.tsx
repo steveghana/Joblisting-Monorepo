b73ca1b231f66c84ce2fba93ccc56fdf4a711c4a
steveghana <boatengstephen707@gmail.com>
2023-12-15 01:47:45 +0000

    Automated Commit file: _update app/web/src/components/auth/AuthCardWrapper.tsx

--- app/web/src/components/auth/AuthCardWrapper.tsx
+++ app/web/src/components/auth/AuthCardWrapper.tsx
@@ -1,28 +1,27 @@
-import PropTypes from "prop-types";
 import { Box } from "@mui/material";
 import MainCard from "../MainCard";
 
 // ==============================|| AUTHENTICATION CARD WRAPPER ||============================== //
 
-const AuthCardWrapper = ({ children, ...other }) => (
+const AuthCardWrapper = ({
+  children,
+  ...other
+}: {
+  children: React.ReactNode;
+}) => (
   <MainCard
-    // sx={{
-    //   maxWidth: { xs: 400, lg: 475 },
-    //   margin: { xs: 2.5, md: 3 },
-    //   "& > *": {
-    //     flexGrow: 1,
-    //     flexBasis: "50%",
-    //   },
-    // }}
+    sx={{
+      margin: { xs: 2.5, md: 3 },
+      "& > *": {
+        flexGrow: 1,
+        flexBasis: "50%",
+      },
+    }}
     content={false}
     {...other}
   >
-    <Box sx={{ p: { xs: 2, sm: 3, xl: 5 } }}>{children}</Box>
+    <Box sx={{ p: { xs: 1, sm: 2, xl: 3 } }}>{children}</Box>
   </MainCard>
 );
-
-AuthCardWrapper.propTypes = {
-  children: PropTypes.node,
-};
 
 export default AuthCardWrapper;
