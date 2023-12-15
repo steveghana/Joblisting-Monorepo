b0b95bb9790709bf6abaebb588dc9ada1b144e78
steveghana <boatengstephen707@gmail.com>
2023-12-15 01:47:40 +0000

    Automated Commit file: _update app/web/src/components/FullscreenProgress/FullscreenProgress.tsx

--- app/web/src/components/FullscreenProgress/FullscreenProgress.tsx
+++ app/web/src/components/FullscreenProgress/FullscreenProgress.tsx
@@ -1,7 +1,30 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress, Grid } from "@mui/material";
 import React from "react";
 
 const FullscreenProgress = () => {
+  return (
+    <div id="preRender">
+      <div className="loader-inner">
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export const TransparentScreeProgress = () => {
   return (
     <div id="preRender">
       <div className="loader-inner">
