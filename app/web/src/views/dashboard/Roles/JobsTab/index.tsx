c91c98c22c6a5f791cb52f31d871d3e32471551a
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:31:13 +0000

    Automated Commit file: _update app/web/src/views/dashboard/Roles/JobsTab/index.tsx

--- app/web/src/views/dashboard/Roles/JobsTab/index.tsx
+++ app/web/src/views/dashboard/Roles/JobsTab/index.tsx
@@ -1,53 +1,20 @@
 // JobsPage.tsx
-import React, { useState } from "react";
-import JobsList from "./joblist";
-import JobFilters from "./jobFilters";
-import {
-  Avatar,
-  Container,
-  Box,
-  Button,
-  ButtonBase,
-  Card,
-  Chip,
-  Divider,
-  Grid,
-  Typography,
-  useMediaQuery,
-  useTheme,
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  FormControlLabel,
-  Checkbox,
-} from "@mui/material";
+import React, { useState } from 'react';
+import JobsList from './joblist';
+import JobFilters from './jobFilters';
+import { Avatar, Container, Box, Grid, Typography, useMediaQuery, useTheme } from '@mui/material';
 // import { roleData } from "./roledata";
-import {
-  ArrowDropDown,
-  ArrowDropUp,
-  ArrowForward,
-  ExpandMore,
-} from "@mui/icons-material";
-import { useNavigate } from "react-router";
-import RoleSummary from "../roleSummary";
-import { IClient } from "../../../../types/client";
-import { IJobs } from "../../../../types";
-import NoData from "../../../../components/NoData";
-import SubCard from "../../../../components/SubCard";
+import RoleSummary from '../roleSummary';
+import { IClient } from '../../../../types/client';
+import { IJobs } from '../../../../types';
+import NoData from '../../../../components/NoData';
+import SubCard from '../../../../components/SubCard';
 // import RoleSummary from "./roleSummary";
-interface Job {
-  id: number;
-  title: string;
-  company: string;
-  location: string;
-}
 
-const JobsPage: React.FC<{ job: IJobs[]; client: IClient; roleId: string }> = (
-  props
-) => {
+const JobsPage: React.FC<{ job: IJobs[]; client: IClient; roleId: string }> = props => {
   const [jobs, setJobs] = useState<IJobs[]>(props.job);
   const theme = useTheme();
-  const matchUpMd = useMediaQuery(theme.breakpoints.up("md"));
+  const matchUpMd = useMediaQuery(theme.breakpoints.up('md'));
   const [expanded, setExpanded] = React.useState<string | false>(false);
 
   const handleFilterChange = (newFilters: Record<string, any>) => {
@@ -56,40 +23,33 @@ const JobsPage: React.FC<{ job: IJobs[]; client: IClient; roleId: string }> = (
     // Applying filtering logic here based on newFilters
     const filteredJobs =
       newFilters[filterkey] === true
-        ? props.job.filter((job) => {
+        ? props.job.filter(job => {
             return job.roleCategory === filterkey;
           })
         : props.job;
     setJobs(filteredJobs);
   };
-  const categoryByfilter = props.job?.map((item) => {
+  const categoryByfilter = props.job?.map(item => {
     //We want to categorise the array by their respective categories
     return {
-      [item.roleCategory]: props.job.filter(
-        (t) => t.roleCategory === item.roleCategory
-      ),
+      [item.roleCategory]: props.job.filter(t => t.roleCategory === item.roleCategory),
     };
   });
   return (
-    <Container sx={{ p: { lg: "inherit", xs: 0 } }}>
-      <Typography my={"1"} variant="h2">
+    <Container sx={{ p: { lg: 'inherit', xs: 0 } }}>
+      <Typography my={'1'} variant="h2">
         Jobs
       </Typography>
       {!jobs.length ? (
         <NoData />
       ) : (
         <SubCard>
-          <Grid
-            display={"flex"}
-            flexWrap={"wrap"}
-            justifyContent={"space-between"}
-            gap={1}
-          >
+          <Grid display={'flex'} flexWrap={'wrap'} justifyContent={'space-between'} gap={1}>
             <Grid>
               <Grid
                 item
-                display={"flex"}
-                flexDirection={"column"}
+                display={'flex'}
+                flexDirection={'column'}
                 // my={2}
                 gap={1}
               >
@@ -100,26 +60,12 @@ const JobsPage: React.FC<{ job: IJobs[]; client: IClient; roleId: string }> = (
                   {props.client.aboutTheCompany}
                 </Typography>
               </Grid>
-              <Box
-                my={2}
-                display={"flex"}
-                justifyContent={"space-between"}
-                flexWrap={"wrap"}
-                alignItems={"flex-start"}
-                sx={{ width: "100%" }}
-              >
-                <JobFilters
-                  filters={categoryByfilter}
-                  onChange={handleFilterChange}
-                />
-                <JobsList
-                  jobs={jobs}
-                  location={props.client.country}
-                  roleId={props.roleId}
-                />
+              <Box my={2} display={'flex'} justifyContent={'space-between'} flexWrap={'wrap'} alignItems={'flex-start'} sx={{ width: '100%' }}>
+                <JobFilters filters={categoryByfilter} onChange={handleFilterChange} />
+                <JobsList jobs={jobs} location={props.client.country} roleId={props.roleId} />
                 {matchUpMd && <RoleSummary client={props.client} />}
               </Box>
-            </Grid>{" "}
+            </Grid>{' '}
           </Grid>
         </SubCard>
       )}
