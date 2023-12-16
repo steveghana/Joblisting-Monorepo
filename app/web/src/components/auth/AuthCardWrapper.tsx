243cb56f24b0f63b65cf1a4c8e4c30545e804fb2
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:30:41 +0000

    Automated Commit file: _update app/web/src/components/auth/AuthCardWrapper.tsx

--- app/web/src/components/auth/AuthCardWrapper.tsx
+++ app/web/src/components/auth/AuthCardWrapper.tsx
@@ -1,27 +1,23 @@
-import { Box } from "@mui/material";
-import MainCard from "../MainCard";
+import { Box } from '@mui/material';
+import MainCard from '../MainCard';
 
 // ==============================|| AUTHENTICATION CARD WRAPPER ||============================== //
 
-const AuthCardWrapper = ({
-  children,
-  ...other
-}: {
-  children: React.ReactNode;
-}) => (
-  <MainCard
-    sx={{
-      margin: { xs: 2.5, md: 3 },
-      "& > *": {
-        flexGrow: 1,
-        flexBasis: "50%",
-      },
-    }}
-    content={false}
-    {...other}
-  >
-    <Box sx={{ p: { xs: 1, sm: 2, xl: 3 } }}>{children}</Box>
-  </MainCard>
+const AuthCardWrapper = ({ children, ...other }: { children: React.ReactNode }) => (
+    <MainCard
+        sx={{
+            margin: { xs: 2.5, md: 3 },
+            '& > *': {
+                flexGrow: 1,
+                flexBasis: '50%',
+            },
+        }}
+        content={false}
+        {...other}
+    >
+        <Box sx={{ p: { xs: 1, sm: 2, xl: 3 } }}>{children}</Box>
+    </MainCard>
 );
 
 export default AuthCardWrapper;
+
