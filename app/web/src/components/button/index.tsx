bc6db27d025bfa8ed4dc2fa8981db6e9cc4a4ce9
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:30:43 +0000

    Automated Commit file: _update app/web/src/components/button/index.tsx

--- app/web/src/components/button/index.tsx
+++ app/web/src/components/button/index.tsx
@@ -1,36 +1,35 @@
-import { Typography } from "@mui/material";
-import LoadingButton, { LoadingButtonProps } from "@mui/lab/LoadingButton";
-import React from "react";
+import { Typography } from '@mui/material';
+import LoadingButton, { LoadingButtonProps } from '@mui/lab/LoadingButton';
+import React from 'react';
 
 interface IButtonProps extends LoadingButtonProps {
-  text: string; // Add your custom text property
-  children?: React.ReactNode;
+    text: string; // Add your custom text property
+    children?: React.ReactNode;
 }
 
 function CustomButton(props: IButtonProps) {
-  return (
-    <LoadingButton
-      {...props} // Spread all properties from IButtonProps
-      // fullWidth
-      // loadingPosition="end"
-      variant={props.variant || "contained"}
-      sx={{
-        my: 2,
-        boxShadow:
-          "rgba(77, 172, 255, 0.45) 0px 13px 27px -5px, rgba(77, 172, 255, 0.4) 0px 8px 16px -8px;",
-        borderRadius: "10px",
-        // color: "white",
-        minWidth: "50px",
-        // background: "red",
-        // background: props.variant === "contained" && themePalette.primary.main,
-      }}
-    >
-      <Typography fontSize={".7rem"} variant="button">
-        {props.text}
-      </Typography>
-      {props.children}
-    </LoadingButton>
-  );
+    return (
+        <LoadingButton
+            {...props}
+            // fullWidth
+            loadingPosition="end"
+            variant={props.variant || 'contained'}
+            sx={{
+                my: 1,
+                boxShadow: 'rgba(77, 172, 255, 0.45) 0px 13px 27px -5px, rgba(77, 172, 255, 0.4) 0px 8px 16px -8px;',
+                borderRadius: '10px',
+                // color: "white",
+                minWidth: '50px',
+                // background: "red",
+                // background: props.variant === "contained" && themePalette.primary.main,
+            }}
+        >
+            <Typography fontSize={'.7rem'} variant="button">
+                {props.text}
+            </Typography>
+            {props.children}
+        </LoadingButton>
+    );
 }
 
 export default CustomButton;
