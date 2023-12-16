96cf22334a1268180561eb2fd91eeddf4fe835bf
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:30:58 +0000

    Automated Commit file: _update app/web/src/routes/AuthenticationRoutes.tsx

--- app/web/src/routes/AuthenticationRoutes.tsx
+++ app/web/src/routes/AuthenticationRoutes.tsx
@@ -1,43 +1,40 @@
-import { lazy } from "react";
-import React from "react";
-import MinimalLayout from "../layout/MinimalLayout";
+import { lazy } from 'react';
+import React from 'react';
+import MinimalLayout from '../layout/MinimalLayout';
 // project imports
-import Loadable from "../components/Loadable";
-import AuthCallBack from "../components/auth/login";
-import RoleAuth from "../components/auth/roleAuthForm";
+import Loadable from '../components/Loadable';
+import AuthCallBack from '../components/auth/login';
+import RoleAuth from '../components/auth/roleAuthForm';
 // import MinimalLayout from 'layout/MinimalLayout';
 
 // login option 3 routing
-const AuthLogin3 = Loadable(
-  lazy(() => import("../components/auth/authentication3/Login3"))
-);
-const AuthRegister3 = Loadable(
-  lazy(() => import("../components/auth/authentication3/Register3"))
-);
+const AuthLogin3 = Loadable(lazy(() => import('../components/auth/authContainer/loginContainer')));
+const AuthRegister3 = Loadable(lazy(() => import('../components/auth/authContainer/registerContainer')));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
 const AuthenticationRoutes = {
-  path: "/",
-  element: <MinimalLayout />,
-  children: [
-    {
-      path: "/role/select",
-      element: <RoleAuth />,
-    },
-    {
-      path: "/auth/login",
-      element: <AuthLogin3 />,
-    },
-    {
-      path: "/auth/login/callback",
-      element: <AuthCallBack />,
-    },
-    {
-      path: "/auth/register",
-      element: <AuthRegister3 />,
-    },
-  ],
+    path: '/',
+    element: <MinimalLayout />,
+    children: [
+        {
+            path: '/role/select',
+            element: <RoleAuth />,
+        },
+        {
+            path: '/auth/login',
+            element: <AuthLogin3 />,
+        },
+        {
+            path: '/auth/login/callback',
+            element: <AuthCallBack />,
+        },
+        {
+            path: '/auth/register',
+            element: <AuthRegister3 />,
+        },
+    ],
 };
 
 export default AuthenticationRoutes;
+
