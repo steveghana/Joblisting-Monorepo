480aada6f9dd893b197b1c647c6d1a4d408eb6c9
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:31:01 +0000

    Automated Commit file: _update app/web/src/store/services/userAuth.service.ts

--- app/web/src/store/services/userAuth.service.ts
+++ app/web/src/store/services/userAuth.service.ts
@@ -1,176 +1,169 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IProfession, IRoleData } from "../../types/roles";
-import axios from "axios";
-import _api_url from "../../api/_api_url";
-import { TokenResponse } from "@react-oauth/google";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IProfession } from '../../types/roles';
+import _api_url from '../../api/_api_url';
+import { IUser } from '../../types/user';
+import { USER_API_KEY } from '../constant';
 
-interface IUser {
-  role?: IProfession;
-  email: string;
-  password: string;
-}
 interface IRegister {
-  user: {
-    firstName: string;
-    lastName: string;
-    role?: IProfession;
-    phoneNumber: string;
-  } & IUser;
+    user: {
+        firstName: string;
+        lastName: string;
+        role?: IProfession;
+        phoneNumber: string;
+        password: string;
+        email: string;
+    };
 }
 type IResponseRg = {
-  token: string;
-  email: string;
-  password: string;
+    token: string;
+    email: string;
+    password: string;
 };
 type IResponseLg = { role: string; authTokenId: string };
 
-export const USER_API_KEY = "userApi";
-
 export const userApi = createApi({
-  reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: _api_url.getApiUrl(),
-    prepareHeaders: (headers, { getState }) => {
-      const authToken = sessionStorage.getItem("auth_token");
-      if (authToken) {
-        headers.set("Authorization", authToken);
-      }
-      return headers;
-    },
-  }),
-  endpoints: (builder) => ({
-    loginUser: builder.mutation<IResponseLg, IUser & { rememberMe: boolean }>({
-      query: (user) => ({
-        url: "user/login",
-        method: "POST",
-        body: user,
-      }),
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-      // providesTags: (result, error, id) => [{ type: "Post", id }],
+    reducerPath: USER_API_KEY,
+    baseQuery: fetchBaseQuery({
+        baseUrl: _api_url.getApiUrl(),
+        prepareHeaders: (headers, { getState }) => {
+            const authToken = sessionStorage.getItem('auth_token');
+            if (authToken) {
+                headers.set('Authorization', authToken);
+            }
+            return headers;
+        },
     }),
-    loginUserWithGoogle: builder.mutation<
-      IResponseLg,
-      { accessToken: string; role?: IProfession }
-    >({
-      query: ({ accessToken }) => ({
-        url: "user/login/google",
-        method: "POST",
-        body: { accessToken },
-      }),
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-      // providesTags: (result, error, id) => [{ type: "Post", id }],
+    endpoints: builder => ({
+        loginUser: builder.mutation<IResponseLg, Omit<IResponseRg, 'token'> & { rememberMe: boolean }>({
+            query: user => ({
+                url: 'user/login',
+                method: 'POST',
+                body: user,
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        loginUserWithGoogle: builder.mutation<IResponseLg, { accessToken: string; role?: IProfession }>({
+            query: ({ accessToken }) => ({
+                url: 'user/login/google',
+                method: 'POST',
+                body: { accessToken },
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        registerUserWithGoogle: builder.mutation<IResponseRg, { accessToken: string }>({
+            query: ({ accessToken }) => ({
+                url: 'user/register/google',
+                method: 'POST',
+                body: { accessToken },
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+            // providesTags: (result, error, id) => [{ type: "Post", id }],
+        }),
+        whoami: builder.query<IUser, void>({
+            query: () => ({
+                url: `user/whoami`,
+                method: 'GET',
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        registerUser: builder.mutation<IResponseRg, IRegister>({
+            query: user => ({
+                url: 'user/register',
+                method: 'POST',
+                body: user,
+            }),
+            // Pick out errors and prevent nested properties in a hook or selector
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        updateUser: builder.mutation<number, { role: IProfession }>({
+            query: user => ({
+                url: 'user/update',
+                method: 'PATCH',
+                body: { ...user },
+            }),
+            // Pick out errors and prevent nested properties in a hook or selector
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        getUser: builder.query<IUser, { id: string }>({
+            query: ({ id }) => ({
+                url: `user/${id}`,
+                method: 'GET',
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
+        getRoles: builder.query<IProfession[], void>({
+            query: () => ({
+                url: `user`,
+                method: 'GET',
+            }),
+            transformErrorResponse: (response: any) => {
+                const {
+                    data: {
+                        error: { message },
+                    },
+                } = response;
+                return Array.isArray(message) ? message.join(',') : message;
+            },
+        }),
     }),
-    registerUserWithGoogle: builder.mutation<
-      IResponseRg,
-      { accessToken: string }
-    >({
-      query: ({ accessToken }) => ({
-        url: "user/register/google",
-        method: "POST",
-        body: { accessToken },
-      }),
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-      // providesTags: (result, error, id) => [{ type: "Post", id }],
-    }),
-    // getAccessToken: builder.query<AuthResponse, string>({
-    //   query: (code) => {
-    //     return {
-    //       url: "github/access_token",
-    //       method: "POST",
-    //       body: { code },
-    //     };
-    //   },
-    // }),
-    registerUser: builder.mutation<IResponseRg, IRegister>({
-      query: (user) => ({
-        url: "user/register",
-        method: "POST",
-        body: user,
-      }),
-      // Pick out errors and prevent nested properties in a hook or selector
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-    }),
-    updateUser: builder.mutation<number, { role: IProfession }>({
-      query: (user) => ({
-        url: "user/update",
-        method: "PATCH",
-        body: { ...user },
-      }),
-      // Pick out errors and prevent nested properties in a hook or selector
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-    }),
-    getUser: builder.query<IUser, { id: string }>({
-      query: ({ id }) => ({
-        url: `user/${id}`,
-        method: "GET",
-      }),
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-    }),
-    getRoles: builder.query<IProfession[], void>({
-      query: () => ({
-        url: `user`,
-        method: "GET",
-      }),
-      transformErrorResponse: (response: any, meta, arg) => {
-        const {
-          data: {
-            error: { message },
-          },
-        } = response;
-        return Array.isArray(message) ? message.join(",") : message;
-      },
-    }),
-  }),
 });
 
 export const {
-  useLoginUserMutation,
-  useLoginUserWithGoogleMutation,
-  useGetRolesQuery,
-  useRegisterUserWithGoogleMutation,
-  useRegisterUserMutation,
-  useUpdateUserMutation,
-  useGetUserQuery,
+    useLoginUserMutation,
+    useWhoamiQuery,
+    useLoginUserWithGoogleMutation,
+    useGetRolesQuery,
+    useRegisterUserWithGoogleMutation,
+    useRegisterUserMutation,
+    useUpdateUserMutation,
+    useGetUserQuery,
 } = userApi;
