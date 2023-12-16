c5f47d386c84a48887ac8087453d61299145b1f1
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:31:17 +0000

    Automated Commit file: _update app/web/src/views/dashboard/clients/Details/overview.tsx

--- app/web/src/views/dashboard/clients/Details/overview.tsx
+++ app/web/src/views/dashboard/clients/Details/overview.tsx
@@ -1,40 +1,25 @@
-import React from "react";
-import { Container, Paper, Grid } from "@mui/material";
-import { Protect } from "../../../../components/auth/requireAuth";
-import NoData from "../../../../components/NoData";
-import { IClient } from "../../../../types/client";
-import { FormDataProvider } from "../../../../utils/Contexts/clientFormContext";
-import ClientDetailsPage from "./clientDetails/clientdetails";
-import { IRoleData } from "../../../../types/roles";
+import React from 'react';
+import { Container, Paper, Grid } from '@mui/material';
+import { Protect } from '../../../../components/auth/requireAuth';
+import NoData from '../../../../components/NoData';
+import { FormDataProvider } from '../../../../utils/Contexts/clientFormContext';
+import ClientDetailsPage from './clientDetails/clientdetails';
+import { IRoleData } from '../../../../types/roles';
 
-const ClientDetailsOverview = ({
-  data,
-  onActionComplete,
-}: {
-  data: { role: IRoleData[]; clientId: string };
-  onActionComplete: () => void;
-}) => {
+const ClientDetailsOverview = ({ data, onActionComplete }: { data: { role: IRoleData[]; clientId: string }; onActionComplete: () => void }) => {
   const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-  console.log(data, "from voervi");
+
+  console.log(data, 'from voervi');
   if (!data) {
     return <NoData />;
   }
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="xl">
       <Grid container spacing={1} mt={2}>
         <Grid item xs={12} sm={12}>
           <Paper elevation={2}>
             <FormDataProvider>
-              <ClientDetailsPage
-                data={data}
-                onActionComplete={onActionComplete}
-              />
+              <ClientDetailsPage data={data} onActionComplete={onActionComplete} />
             </FormDataProvider>
           </Paper>
         </Grid>
@@ -42,4 +27,4 @@ const ClientDetailsOverview = ({
     </Container>
   );
 };
-export default Protect(ClientDetailsOverview, ["Ceo"]);
+export default Protect(ClientDetailsOverview, ['Ceo']);
