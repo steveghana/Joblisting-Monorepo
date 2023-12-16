dd12120244106a909aa93e39f72c77fccc92a8e8
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:31:07 +0000

    Automated Commit file: _update app/web/src/views/Devs/Devs.tsx

--- app/web/src/views/Devs/Devs.tsx
+++ app/web/src/views/Devs/Devs.tsx
@@ -1,43 +1,42 @@
-import React, { useEffect } from "react";
-import FullscreenProgress from "../../components/FullscreenProgress/FullscreenProgress";
-import NoData from "../../components/NoData";
-import { useDevsColums } from "../../hooks/useAllDevsColumn";
-import DevTableData from "./DevColumns";
-import { useGetDevsQuery } from "../../store/services/dev.service";
+import React, { useEffect } from 'react';
+import FullscreenProgress from '../../components/FullscreenProgress/FullscreenProgress';
+import NoData from '../../components/NoData';
+import { useDevsColums } from '../../hooks/useAllDevsColumn';
+import DevTableData from './DevColumns';
+import { useGetDevsQuery } from '../../store/services/dev.service';
+import MainCard from '../../components/MainCard';
+import { Grid } from '@mui/material';
 
 const Developers = () => {
   const columns = useDevsColums();
 
-  const {
-    data: devs,
-    isError,
-    isLoading,
-    isFetching,
-    refetch,
-  } = useGetDevsQuery();
-  const devsData =
-    (devs?.length &&
-      devs?.filter(({ rolestatus }) => rolestatus === "Accepted")) ||
-    [];
+  const { data: devs, isError, isLoading, isFetching, refetch } = useGetDevsQuery();
+  const devsData = (devs?.length && devs?.filter(({ rolestatus }) => rolestatus === 'Accepted')) || [];
 
   if (isLoading || isFetching) {
     return <FullscreenProgress />;
   }
 
   if (!devsData.length || isError) {
-    return <NoData />;
+    return (
+      <Grid height={'100%'} sx={{ background: 'white' }}>
+        <NoData />
+      </Grid>
+    );
   }
   console.log(devsData);
   return (
-    <DevTableData
-      columns={columns}
-      devs={devsData}
-      // You can still use refetch if needed
-      refetch={() => refetch()}
-      isLoading={isLoading}
-      isError={isError}
-      isFetching={isFetching}
-    />
+    <MainCard>
+      <DevTableData
+        columns={columns}
+        devs={devsData}
+        // You can still use refetch if needed
+        refetch={() => refetch()}
+        isLoading={isLoading}
+        isError={isError}
+        isFetching={isFetching}
+      />
+    </MainCard>
   );
 };
 
