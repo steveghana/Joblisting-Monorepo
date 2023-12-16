9491169c3763a0df83161aebfa628a84259f5351
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:30:52 +0000

    Automated Commit file: _update app/web/src/hooks/useAllDevsColumn.tsx

--- app/web/src/hooks/useAllDevsColumn.tsx
+++ app/web/src/hooks/useAllDevsColumn.tsx
@@ -1,133 +1,132 @@
-import { MRT_ColumnDef } from "material-react-table";
-import { useMemo } from "react";
-import { IDev } from "../types/devs";
-import { Avatar, Box } from "@mui/material";
+import { MRT_ColumnDef } from 'material-react-table';
+import { useMemo } from 'react';
+import { IDev } from '../types/devs';
+import { Avatar, Box } from '@mui/material';
 export const useDevsColums = () => {
-  return useMemo<MRT_ColumnDef<IDev>[]>(
-    () => [
-      {
-        id: "employee", //id used to define `group` column
-        header: "Employee",
-        columns: [
-          {
-            accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
-            id: "name", //id is still required when using accessorFn instead of accessorKey
-            header: "Name",
-            enableEditing: false,
+    return useMemo<MRT_ColumnDef<IDev>[]>(
+        () => [
+            {
+                id: 'employee', //id used to define `group` column
+                header: 'Employee',
+                columns: [
+                    {
+                        accessorFn: row => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
+                        id: 'name', //id is still required when using accessorFn instead of accessorKey
+                        header: 'Name',
+                        enableEditing: false,
 
-            size: 200,
-            Cell: ({ renderedCellValue, row }) => (
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "1rem",
-                }}
-              >
-                <Avatar src={row.original.avatar} />
+                        size: 200,
+                        Cell: ({ renderedCellValue, row }) => (
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    gap: '1rem',
+                                }}
+                            >
+                                <Avatar src={row.original.avatar} />
 
-                {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
-                <span>{renderedCellValue}</span>
-              </Box>
-            ),
-          },
-          {
-            header: "Status",
-            accessorFn: (originalRow) => originalRow.workStatus, //must be strings
-            id: "isActive",
-            filterVariant: "select",
-            Cell: ({ cell }) => <Box>{cell.getValue<string>()}</Box>,
-            size: 100,
-          },
-          {
-            accessorKey: "email", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-            enableClickToCopy: true,
-            enableEditing: false,
+                                {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
+                                <span>{renderedCellValue}</span>
+                            </Box>
+                        ),
+                    },
+                    {
+                        header: 'Status',
+                        accessorFn: originalRow => originalRow.workStatus, //must be strings
+                        id: 'isActive',
+                        filterVariant: 'select',
+                        Cell: ({ cell }) => <Box>{cell.getValue<string>()}</Box>,
+                        size: 100,
+                    },
+                    {
+                        accessorKey: 'email', //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                        enableClickToCopy: true,
+                        enableEditing: false,
 
-            filterVariant: "autocomplete",
-            header: "Email",
-            size: 100,
-          },
-          {
-            id: "clientName",
-            accessorKey: "clientName", //hey a simple column for once
-            header: "Client Name",
-            enableEditing: false,
-            size: 200,
-          },
-          {
-            id: "companyName",
-            accessorKey: "companyName", //hey a simple column for once
-            header: "Company Name",
-            enableEditing: false,
-            size: 200,
-          },
-          {
-            accessorKey: "projectName", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-            enableClickToCopy: true,
-            filterVariant: "autocomplete",
-            enableEditing: false,
+                        filterVariant: 'autocomplete',
+                        header: 'Email',
+                        size: 100,
+                    },
+                    {
+                        id: 'clientName',
+                        accessorKey: 'clientName', //hey a simple column for once
+                        header: 'Client Name',
+                        enableEditing: false,
+                        size: 200,
+                    },
+                    {
+                        id: 'companyName',
+                        accessorKey: 'companyName', //hey a simple column for once
+                        header: 'Company Name',
+                        enableEditing: false,
+                        size: 200,
+                    },
+                    {
+                        accessorKey: 'projectName', //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                        enableClickToCopy: true,
+                        filterVariant: 'autocomplete',
+                        enableEditing: false,
 
-            header: "Project Name",
-            size: 150,
-          },
-        ],
-      },
-      {
-        id: "id",
-        header: "Job Info",
-        columns: [
-          {
-            accessorKey: "salary",
-            size: 100,
-
-            header: "Salary",
-            enableEditing: true,
-            Cell: ({ cell }) => (
-              <Box
-                component="span"
-                sx={(theme) => ({
-                  backgroundColor:
-                    cell.getValue<number>() < 50_000
-                      ? theme.palette.error.dark
-                      : cell.getValue<number>() >= 50_000 &&
-                        cell.getValue<number>() < 75_000
-                      ? theme.palette.warning.dark
-                      : theme.palette.success.dark,
-                  borderRadius: "0.25rem",
-                  color: "#fff",
-                  maxWidth: "9ch",
-                  p: "0.25rem",
-                })}
-              >
-                {cell.getValue<number>()?.toLocaleString?.("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                })}
-              </Box>
-            ),
-            filterFn: "between", // default (or between)
-          },
-          {
-            accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
-            id: "startDate",
-            header: "Start Date",
-            filterVariant: "date",
-            filterFn: "lessThan",
-            sortingFn: "datetime",
-            Cell: ({ cell }) => cell.getValue<Date>()?.toLocaleDateString(), //render Date as a string
-            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
-            muiFilterTextFieldProps: {
-              sx: {
-                minWidth: "250px",
-              },
+                        header: 'Project Name',
+                        size: 150,
+                    },
+                ],
             },
-          },
+            {
+                id: 'id',
+                header: 'Job Info',
+                columns: [
+                    {
+                        accessorKey: 'salary',
+                        size: 100,
+
+                        header: 'Salary',
+                        enableEditing: true,
+                        Cell: ({ cell }) => (
+                            <Box
+                                component="span"
+                                sx={theme => ({
+                                    backgroundColor:
+                                        cell.getValue<number>() < 50_000
+                                            ? theme.palette.error.dark
+                                            : cell.getValue<number>() >= 50_000 && cell.getValue<number>() < 75_000
+                                            ? theme.palette.warning.dark
+                                            : theme.palette.success.dark,
+                                    borderRadius: '0.25rem',
+                                    color: '#fff',
+                                    maxWidth: '9ch',
+                                    p: '0.25rem',
+                                })}
+                            >
+                                {cell.getValue<number>()?.toLocaleString?.('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                    minimumFractionDigits: 0,
+                                    maximumFractionDigits: 0,
+                                })}
+                            </Box>
+                        ),
+                        filterFn: 'between', // default (or between)
+                    },
+                    {
+                        accessorFn: row => new Date(row.startDate), //convert to Date for sorting and filtering
+                        id: 'startDate',
+                        header: 'Start Date',
+                        filterVariant: 'date',
+                        filterFn: 'lessThan',
+                        sortingFn: 'datetime',
+                        Cell: ({ cell }) => cell.getValue<Date>()?.toLocaleDateString(), //render Date as a string
+                        Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+                        muiFilterTextFieldProps: {
+                            sx: {
+                                minWidth: '250px',
+                            },
+                        },
+                    },
+                ],
+            },
         ],
-      },
-    ],
-    []
-  );
+        []
+    );
 };
