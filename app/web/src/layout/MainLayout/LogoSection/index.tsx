1f062be9102d6d1604b109bb02c33bc350197e10
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:30:54 +0000

    Automated Commit file: _update app/web/src/layout/MainLayout/LogoSection/index.tsx

--- app/web/src/layout/MainLayout/LogoSection/index.tsx
+++ app/web/src/layout/MainLayout/LogoSection/index.tsx
@@ -1,49 +1,39 @@
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 // material-ui
-import { Box, ButtonBase, IconButton } from "@mui/material";
-import LogoImg from "../../../assets/images/Logo-Small-19.png";
+import { Box, ButtonBase, IconButton } from '@mui/material';
+import LogoImg from '../../../assets/images/Logo-Small-19.png';
 // project imports
-import config from "../../../config";
-import { MENU_OPEN } from "../../../store/actions";
+import config from '../../../config';
+import { MENU_OPEN } from '../../../store/actions';
 
 // ==============================|| MAIN LOGO ||============================== //
 
 const LogoSection = () => {
-  const defaultId = useSelector((state: any) => state.customization?.defaultId);
-  console.log(defaultId, "from log");
-  const lockSidebar = false;
-  const dispatch = useDispatch();
-  if (lockSidebar) {
+    const defaultId = useSelector((state: any) => state.customization?.defaultId);
+    console.log(defaultId, 'from log');
+    const hasToken = sessionStorage.getItem('auth_token');
+    const lockSidebar = false;
+    const dispatch = useDispatch();
+    if (!hasToken) {
+        return (
+            <ButtonBase disableRipple>
+                <img src={LogoImg} style={{ objectFit: 'contain' }} alt="Logo" width={100} height={50} />
+            </ButtonBase>
+        );
+    }
     return (
-      <ButtonBase disableRipple>
-        <img
-          src={LogoImg}
-          style={{ objectFit: "contain" }}
-          alt="Logo"
-          width={100}
-          height={50}
-        />
-      </ButtonBase>
+        <ButtonBase
+            disableRipple
+            onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })}
+            component={Link}
+            to={config.defaultPath}
+        >
+            <img src={LogoImg} style={{ objectFit: 'contain' }} alt="Logo" width={100} height={50} />
+        </ButtonBase>
     );
-  }
-  return (
-    <ButtonBase
-      disableRipple
-      onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })}
-      component={Link}
-      to={config.defaultPath}
-    >
-      <img
-        src={LogoImg}
-        style={{ objectFit: "contain" }}
-        alt="Logo"
-        width={100}
-        height={50}
-      />
-    </ButtonBase>
-  );
 };
 
 export default LogoSection;
+
