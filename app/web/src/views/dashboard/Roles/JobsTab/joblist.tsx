c01b04ae40c01b31cdabf903b7d74ed626b78f0b
steveghana <boatengstephen707@gmail.com>
2023-12-16 14:31:13 +0000

    Automated Commit file: _update app/web/src/views/dashboard/Roles/JobsTab/joblist.tsx

--- app/web/src/views/dashboard/Roles/JobsTab/joblist.tsx
+++ app/web/src/views/dashboard/Roles/JobsTab/joblist.tsx
@@ -1,26 +1,15 @@
 // JobsList.tsx
-import React from "react";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  Grid,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
-import { ClockIcon } from "@mui/x-date-pickers";
-import { IJobs } from "../../../../types";
-import { themePalette } from "../../../../themes/schemes/palette";
-import Dot from "../../../../components/Dot";
-import { FiberManualRecord } from "@mui/icons-material";
-import NoData from "../../../../components/NoData";
-import { formatTimeDifference } from "../../../../utils/timeFormatter";
-import SubCard from "../../../../components/SubCard";
-import CustomButton from "../../../../components/button";
+import React from 'react';
+import { Box, Button, Card, CardContent, Grid, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { ClockIcon } from '@mui/x-date-pickers';
+import { IJobs } from '../../../../types';
+import { themePalette } from '../../../../themes/schemes/palette';
+import Dot from '../../../../components/Dot';
+import { FiberManualRecord } from '@mui/icons-material';
+import NoData from '../../../../components/NoData';
+import { formatTimeDifference } from '../../../../utils/timeFormatter';
+import SubCard from '../../../../components/SubCard';
+import CustomButton from '../../../../components/button';
 
 interface JobsListProps {
   jobs: IJobs[];
@@ -35,42 +24,31 @@ const JobsList: React.FC<JobsListProps> = ({ jobs, location, roleId }) => {
       {!jobs?.length ? (
         <NoData />
       ) : (
-        jobs?.map((job) => (
+        jobs?.map(job => (
           <SubCard key={job.id}>
             {/* <CardContent> */}
-            <Box display={"flex"} justifyContent={"space-between"}>
+            <Box display={'flex'} justifyContent={'space-between'}>
               <Box>
-                <Typography color={"gray"} variant="subtitle1">
+                <Typography color={'gray'} variant="subtitle1">
                   {job.roleCategory}
                 </Typography>
-                <Typography color={themePalette.primary.main}>
-                  {job.roleName}
-                </Typography>
+                <Typography color={themePalette.primary.main}>{job.roleName}</Typography>
               </Box>
-              <Box
-                display={"flex"}
-                flexDirection={"column"}
-                justifyContent={"flex-start"}
-              >
-                <ClockIcon
-                  fontSize="small"
-                  sx={{ ml: "auto" }}
-                  color="disabled"
-                />
+              <Box display={'flex'} flexDirection={'column'} justifyContent={'flex-start'}>
+                <ClockIcon fontSize="small" sx={{ ml: 'auto' }} color="disabled" />
                 {/* use the date */}
-                <Typography whiteSpace={"nowrap"} variant="caption">
-                  Posted {formatTimeDifference(now, new Date(job.postedDate))}{" "}
-                  ago
+                <Typography whiteSpace={'nowrap'} variant="caption">
+                  Posted {formatTimeDifference(now, new Date(job.postedDate))} ago
                 </Typography>
               </Box>
             </Box>
             <Box>
               <Typography>{job.description}</Typography>
               <List>
-                {job.tasks.map((t) => (
-                  <ListItem sx={{ display: "flex" }}>
+                {job.tasks.map(t => (
+                  <ListItem sx={{ display: 'flex' }}>
                     <ListItemIcon>
-                      <FiberManualRecord sx={{ fontSize: ".5rem" }} />
+                      <FiberManualRecord sx={{ fontSize: '.5rem' }} />
                     </ListItemIcon>
                     <ListItemText primary={t} />
                   </ListItem>
@@ -80,21 +58,23 @@ const JobsList: React.FC<JobsListProps> = ({ jobs, location, roleId }) => {
 
             <Box
               my={2}
-              alignItems={"center"}
-              gap={".8rem"}
+              alignItems={'flex-start'}
+              justifyContent={'flex-start'}
+              width={'100%'}
+              gap={'.8rem'}
               borderRadius={2}
               p={1}
-              border={"2px solid rgba(0, 0, 0, 0.1)"}
-              display={"flex"}
-              flexWrap={"wrap"}
+              border={'2px solid rgba(0, 0, 0, 0.1)'}
+              display={'flex'}
+              flexWrap={'wrap'}
             >
               <Typography fontWeight={700} variant="body2">
                 {job.roleName}
               </Typography>
-              <Box display={"flex"} alignItems={"center"} gap={0.4}>
+              <Box display={'flex'} alignItems={'center'} gap={0.4}>
                 <Dot />
-                <Typography fontWeight={400} variant="subtitle1" mr={"auto"}>
-                  {location.label}{" "}
+                <Typography fontWeight={400} variant="subtitle1" mr={'auto'}>
+                  {location.label}{' '}
                 </Typography>
                 <Dot />
                 <Typography>{job.jobType}</Typography>
@@ -103,48 +83,38 @@ const JobsList: React.FC<JobsListProps> = ({ jobs, location, roleId }) => {
               </Box>
               <Box
                 sx={{
-                  ml: { md: 0, lg: "auto" },
-                  flexDirection: { sm: "column", md: "row" },
-                  justifyContent: { sm: "flex-end", md: "flex-start" },
+                  // ml: { md: 0, lg: 'auto' },
+                  flexDirection: { sm: 'column', md: 'row' },
+                  justifyContent: 'flex-start',
                 }}
-                display={"flex"}
-                justifyContent={"flex-start"}
-                flexWrap={"wrap"}
-                alignItems={"center"}
+                display={'flex'}
+                justifyContent={'flex-start'}
+                flexWrap={'wrap'}
+                // alignItems={'center'}
                 // flexDirection={props.feature ? "column" : "row"}
                 gap={1}
               >
                 <Box
-                  display={"flex"}
-                  flexDirection={"column"}
+                  display={'flex'}
+                  flexDirection={'column'}
                   sx={{
-                    justifyContent: { md: "flex-start", lg: "flex-end" },
-                    alignItems: { md: "flex-start", lg: "flex-end" },
+                    justifyContent: { md: 'flex-start', lg: 'flex-end' },
+                    alignItems: { md: 'flex-start', lg: 'flex-end' },
                   }}
                 >
                   <Typography>
                     <ClockIcon
                       sx={{
                         color: themePalette.primary.main,
-                        fontSize: ".7rem",
+                        fontSize: '.7rem',
                         mr: 0.2,
                       }}
                     />
-                    Posted {formatTimeDifference(now, new Date(job.postedDate))}{" "}
-                    ago
+                    Posted {formatTimeDifference(now, new Date(job.postedDate))} ago
                   </Typography>
                 </Box>
-                <Grid
-                  display={"flex"}
-                  justifyContent={"center"}
-                  sx={{ width: { xs: "100%" } }}
-                >
-                  <CustomButton
-                    size="small"
-                    href={`/job-submttion/${roleId}`}
-                    fullWidth
-                    text="Apply"
-                  />
+                <Grid sx={{ width: { xs: '100%' } }}>
+                  <CustomButton size="medium" fullWidth href={`/job-submttion/${roleId}`} text="Apply" />
                 </Grid>
               </Box>
             </Box>
