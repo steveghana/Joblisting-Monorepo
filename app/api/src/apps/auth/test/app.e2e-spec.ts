94acc2bc3589b4a5d56720ca034a40d2a980c566
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:25:29 +0000

    Automated Commit file: _update app/api/src/apps/auth/test/app.e2e-spec.ts

--- app/api/src/apps/auth/test/app.e2e-spec.ts
+++ app/api/src/apps/auth/test/app.e2e-spec.ts
@@ -1,73 +1,122 @@
+import { AppModule } from '../../../apps/api-gateway/api-gateway.module';
+import { TestDBInitiator } from '../../../apps/api-gateway/test/test.database.module';
+import { createTestDataSource } from '@/apps/api-gateway/test/test.setup';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
-import {
-  setupTestApp,
-  closeTestDatabase,
-  setupTestDatabase,
-} from '../../../apps/api-gateway/test/test.setup';
-import * as supertest from 'supertest';
-import { INestApplication } from '@nestjs/common';
-import { DataSourceOptions } from 'typeorm';
-import Entities from '../../../Config/model';
-import { TestAuthModule } from './auth.module';
+// Import Supertest
+import * as request from 'supertest';
+import { UserEntity } from '../models/user.entity';
+import { IUser } from '@/types/user';
 
-describe('AuthController (e2e)', () => {
+describe('authcontroller (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
+  let databaseConfig: TestDBInitiator;
+  const authUrl = `http://localhost:5000/auth`;
 
+  const mockUser: IUser = {
+    firstName: 'givenName',
+    role: 'Ceo',
+    lastName: 'familyName',
+    email: 'email@homtail.com',
+    password: 'password',
+  };
   beforeAll(async () => {
-    const testDatabaseOptions: DataSourceOptions = {
-      type: 'sqlite',
-      database: ':memory:',
-      entities: [...Entities],
-      synchronize: true,
-      logging: false,
-    };
+    databaseConfig = new TestDBInitiator();
+    dataSource = await createTestDataSource(databaseConfig.dbOptions);
 
-    app = await setupTestApp(testDatabaseOptions, [
-      TestAuthModule.forTesting(),
-    ]);
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...databaseConfig.dbOptions,
+          autoLoadEntities: true,
+        }),
+        AppModule,
+      ],
+    }).compile();
 
-    await setupTestDatabase(testDatabaseOptions);
-  }, 10000);
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
 
   afterAll(async () => {
+    await dataSource.destroy();
     await app.close();
+  });
 
-    const testDatabaseOptions: DataSourceOptions = {
-      type: 'sqlite',
-      database: ':memory:',
-      entities: [...Entities],
-      synchronize: true,
-      logging: false,
+  it('it should register a user and return the new user object', () => {
+    return request(authUrl)
+      .post('/register')
+      .set('Accept', 'application/json')
+      .send(mockUser)
+      .expect((response: request.Response) => {
+        const { email, password, token } = response.body;
+
+        expect(email).toEqual(mockUser.email), expect(password).toBeUndefined();
+        expect(token).toBeDefined();
+      })
+      .expect(HttpStatus.CREATED);
+  });
+  it('/user/register (POST) should register a new user', async () => {
+    const data = {
+      user: {
+        email: 'test@example.com',
+        password: 'password123',
+        firstName: 'John',
+        lastName: 'Doe',
+        role: 'Recruitment',
+        country: 'US',
+      },
     };
-    await closeTestDatabase(testDatabaseOptions);
+
+    const response = await request(app.getHttpServer())
+      .post('/user/register')
+      .send(data)
+      .expect((response: request.Response) => {
+        const { email, password, token } = response.body;
+
+        expect(email).toEqual(data.user.email),
+          expect(password).toBeUndefined();
+        expect(token).toBeDefined();
+        expect(response.body).toHaveProperty('token');
+      })
+      .expect(HttpStatus.CREATED);
+
+    // Assuming you have a user entity in your database
+    // const userRepository = dataSource.getRepository(User);
+    // const createdUser = await userRepository.findOne({
+    //   email: data.user.email,
+    // });
+    // expect(createdUser).toBeDefined();
+    // expect(createdUser.firstName).toEqual(data.user.firstName);
   });
 
-  describe('POST /auth/login', () => {
-    it('should return a JWT token when valid credentials are provided', () => {
-      return supertest(app.getHttpServer())
-        .post('/auth/login')
-        .send({
-          email: 'test@example.com',
-          password: 'password123',
-        })
-        .expect(200)
-        .then((response) => {
-          // Add assertions based on your application logic and expectations
-          expect(response.body).toHaveProperty('token');
-        });
-    });
+  it('/user/login (POST) should login a user', async () => {
+    const data = {
+      email: 'test@example.com',
+      password: 'password123',
+      role: 'Recruitment',
+      rememberMe: false,
+    };
 
-    it('should return an error when invalid credentials are provided', () => {
-      return supertest(app.getHttpServer())
-        .post('/auth/login')
-        .send({
-          email: 'invalid@example.com',
-          password: 'invalidpassword',
-        })
-        .expect(401);
-    });
+    const response = await request(app.getHttpServer())
+      .post('/user/login')
+      .send(data)
+      .expect((response: request.Response) => {
+        const { email, token, role } = response.body;
+
+        expect(email).toEqual(data.email),
+          expect(role).toEqual(data.role),
+          expect(token).toBeDefined();
+        expect(response.body).toHaveProperty('token');
+      })
+      .expect(HttpStatus.CREATED);
+    // Assuming that your AuthService returns some response object
+    expect(response.body).toHaveProperty('token');
   });
 
-  // Add more test cases for other AuthController endpoints (e.g., register, logout)
+  // Add more test cases for other controller methods as needed
 });
