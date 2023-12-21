fb0c8274ba983bcd6486a3d37b928acde07d2019
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:26:11 +0000

    Automated Commit file: _update app/web/src/components/Drawer/index.tsx

--- app/web/src/components/Drawer/index.tsx
+++ app/web/src/components/Drawer/index.tsx
@@ -1,34 +1,30 @@
-import React from "react";
-import { Box, Drawer, useMediaQuery, useTheme } from "@mui/material";
-import PerfectScrollbar from "react-perfect-scrollbar";
+import React from 'react';
+import { Box, Drawer, useMediaQuery, useTheme } from '@mui/material';
+import PerfectScrollbar from 'react-perfect-scrollbar';
 
 type ICustomDrawer = {
   children: React.ReactElement;
   component: React.ReactElement;
   openDrawer: {
-    bottom: boolean;
+    right: boolean;
   };
   setOpenDrawer: React.Dispatch<
     React.SetStateAction<{
-      bottom: boolean;
+      right: boolean;
     }>
   >;
 };
 const CustomDrawer = (props: ICustomDrawer) => {
   const theme = useTheme();
-  const matchUpMd = useMediaQuery(theme.breakpoints.up("md"));
+  const matchUpMd = useMediaQuery(theme.breakpoints.up('md'));
   const list = () => (
     <Box
-      sx={{ width: "auto" }}
+      sx={{ width: 'auto' }}
       px={2.5}
       py={2}
       role="presentation"
-      onClick={() =>
-        props.setOpenDrawer({ ...props.openDrawer, ["bottom"]: false })
-      }
-      onKeyDown={() =>
-        props.setOpenDrawer({ ...props.openDrawer, ["bottom"]: false })
-      }
+      onClick={() => props.setOpenDrawer({ ...props.openDrawer, ['right']: false })}
+      onKeyDown={() => props.setOpenDrawer({ ...props.openDrawer, ['right']: false })}
     >
       {props.component}
     </Box>
@@ -37,25 +33,23 @@ const CustomDrawer = (props: ICustomDrawer) => {
   return (
     <>
       <Drawer
-        anchor={"bottom"}
-        open={props.openDrawer["bottom"]}
-        onClose={() =>
-          props.setOpenDrawer({ ...props.openDrawer, ["bottom"]: false })
-        }
+        anchor={'right'}
+        open={props.openDrawer['right']}
+        onClose={() => props.setOpenDrawer({ ...props.openDrawer, ['right']: false })}
       >
         <Box
           sx={{
-            borderRadius: "35px 35px 0 0",
-            background: "white",
+            borderRadius: '35px 35px 0 0',
+            background: 'white',
           }}
-          height={"89vh"}
+          height={'89vh'}
         >
           <PerfectScrollbar
             component="div"
             style={{
-              height: !matchUpMd ? "calc(89vh - 16px)" : "calc(89vh - 18px)",
-              paddingLeft: "16px",
-              paddingRight: "16px",
+              height: !matchUpMd ? 'calc(89vh - 16px)' : 'calc(89vh - 18px)',
+              paddingLeft: '16px',
+              paddingRight: '16px',
             }}
           >
             {list()}
