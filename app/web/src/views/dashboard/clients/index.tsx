9d982dfb7b2791e7fe1f428443301cc30660fcd3
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:27:14 +0000

    Automated Commit file: _update app/web/src/views/dashboard/clients/index.tsx

--- app/web/src/views/dashboard/clients/index.tsx
+++ app/web/src/views/dashboard/clients/index.tsx
@@ -1,133 +1,50 @@
-import {
-  Avatar,
-  Button,
-  ButtonBase,
-  Divider,
-  Grid,
-  GridTypeMap,
-  Typography,
-} from "@mui/material";
-import MainCard from "../../../components/MainCard";
-import SubCard from "../../../components/SubCard";
-import { Box } from "@mui/system";
-import { BlockOutlined, MessageRounded, MoreHoriz } from "@mui/icons-material";
-import CustomButton from "../../../components/button";
-import { useNavigate } from "react-router";
-import { Protect } from "../../../components/auth/requireAuth";
-import ClientTable from "./Details/clientColumns";
-import { useGetClientsQuery } from "../../../store/services/client.service";
-import NoData from "../../../components/NoData";
-import FullscreenProgress from "../../../components/FullscreenProgress/FullscreenProgress";
+import { Avatar, Button, ButtonBase, Divider, Grid, GridTypeMap, Typography } from '@mui/material';
+import MainCard from '../../../components/MainCard';
+import SubCard from '../../../components/SubCard';
+import { Box } from '@mui/system';
+import { BlockOutlined, MessageRounded, MoreHoriz } from '@mui/icons-material';
+import CustomButton from '../../../components/button';
+import { useNavigate } from 'react-router';
+import { Protect } from '../../../components/auth/requireAuth';
+import ClientTable from './Tables/clientTable';
+import { useGetClientsQuery } from '../../../store/services/client.service';
+import NoData from '../../../components/NoData';
+import FullscreenProgress from '../../../components/FullscreenProgress/FullscreenProgress';
+/**
+ * Clients component displays a list of clients from the API.
+ * It fetches the clients data and handles loading/error states.
+ * Renders a table of clients if data is available,
+ * otherwise shows loading indicator or empty state.
+ * Allows navigating to add a new client.
+ * Exported as default for use in route definitions.
+ */
 
 const Clients = () => {
   const navigate = useNavigate();
-  const { data, isLoading, isFetching, isError, refetch } =
-    useGetClientsQuery();
 
-  if (isLoading || isFetching) {
-    return <FullscreenProgress />;
-  }
+  const { data, isLoading, isFetching, isError, refetch } = useGetClientsQuery();
+
+  const renderContent = () => {
+    if (isLoading || isFetching) {
+      return <FullscreenProgress />;
+    }
+
+    if (data?.length) {
+      return (
+        <ClientTable refetch={refetch} data={data} isError={isError} isFetching={isFetching} isLoading={isLoading} />
+      );
+    }
+
+    return <NoData />;
+  };
+
   return (
-    <MainCard title={"Clients"}>
-      <CustomButton
-        text="Add new Client"
-        onClick={() => navigate("/dashboard/customers/clients/add")}
-      />
+    <MainCard title={'Clients'}>
+      <CustomButton text="Add new Client" onClick={() => navigate('/dashboard/customers/clients/add')} />
 
-      {data?.length ? (
-        <ClientTable
-          refetch={() => refetch()}
-          data={data}
-          isError={isError}
-          isFetching={isFetching}
-          isLoading={isLoading}
-        />
-      ) : (
-        <NoData />
-      )}
+      {renderContent()}
     </MainCard>
   );
 };
 
-const ClientCard = (props) => {
-  return (
-    <SubCard>
-      <Grid container direction="column" spacing={1}>
-        <Grid
-          className="avatar"
-          display={"flex"}
-          gap={"1rem"}
-          justifyContent={"space-between"}
-          item
-        >
-          <Avatar alt="user" variant="rounded" />
-
-          <ButtonBase>
-            <MoreHoriz />
-          </ButtonBase>
-        </Grid>
-        {/* <Divider sx={{ margin: "1rem 0" }} /> */}
-        <Grid my={2} className="mail links" item>
-          <Box>
-            <Typography fontWeight={700} variant="h4" mr={"auto"}>
-              Dillion
-            </Typography>
-            <Typography variant="caption">Fullstack developer</Typography>
-          </Box>
-          {/* <Divider sx={{ margin: "1rem 0" }} /> */}
-          <Box my={2} alignItems={"center"} gap={".4rem"} display={"flex"}>
-            <Typography fontWeight={700} variant="caption" mr={"auto"}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Consectetur nam nulla alias cupiditate molestias, voluptatibus
-              consequatur architecto voluptas
-            </Typography>
-          </Box>
-          <Divider sx={{ margin: "1rem 0" }} />
-
-          <Box>
-            <Box my={2}>
-              <Typography fontWeight={700} variant="h5" mr={"auto"}>
-                Email
-              </Typography>
-              <Typography variant="caption">demo@svtech.com</Typography>
-            </Box>
-            <Grid my={2} display={"flex"} justifyContent={"space-between"}>
-              <Box>
-                <Typography fontWeight={700} variant="h5" mr={"auto"}>
-                  Phone
-                </Typography>
-                <Typography variant="caption">+244 444555444</Typography>
-              </Box>
-              <Box>
-                <Typography fontWeight={700} variant="h5" mr={"auto"}>
-                  Location
-                </Typography>
-                <Typography variant="caption">Melbourne</Typography>
-              </Box>
-            </Grid>
-          </Box>
-          <Divider sx={{ margin: "1rem 0" }} />
-          <Box display={"flex"} justifyContent={"space-between"}>
-            <Button
-              size="small"
-              variant="outlined"
-              startIcon={<MessageRounded />}
-            >
-              Message
-            </Button>
-            <Button
-              size="small"
-              color="error"
-              variant="outlined"
-              startIcon={<BlockOutlined />}
-            >
-              Remove
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
-    </SubCard>
-  );
-};
-
-export default Protect(Clients, ["Ceo"]);
+export default Protect(Clients, ['Ceo']);
