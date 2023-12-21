5e1a0f9420f563d074a9312b1acf75288c83e09f
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:26:39 +0000

    Automated Commit file: _update app/web/src/main.tsx

--- app/web/src/main.tsx
+++ app/web/src/main.tsx
@@ -1,19 +1,15 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "nprogress/nprogress.css";
-import "../src/assets/scss/style.scss";
-import App from "./App";
-import { Provider } from "react-redux";
-import store, { persistor } from "./store";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { PersistGate } from "redux-persist/integration/react";
-import FullscreenProgress from "./components/FullscreenProgress/FullscreenProgress";
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider
-    clientId={
-      "98962994226-v6lsqu50ttq5jgtnu2qs8eucishhej5n.apps.googleusercontent.com"
-    }
-  >
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'nprogress/nprogress.css';
+import '../src/assets/scss/style.scss';
+import App from './App';
+import { Provider } from 'react-redux';
+import store, { persistor } from './store';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { PersistGate } from 'redux-persist/integration/react';
+import FullscreenProgress from './components/FullscreenProgress/FullscreenProgress';
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID as string}>
     <Provider store={store}>
       <BrowserRouter>
         <PersistGate /* s */ persistor={persistor}>
@@ -21,5 +17,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </PersistGate>
       </BrowserRouter>
     </Provider>
-  </GoogleOAuthProvider>
+  </GoogleOAuthProvider>,
 );
