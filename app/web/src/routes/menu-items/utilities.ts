83ab9b56b2d2f6b231eff5c88b9c98ad008caec4
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:26:41 +0000

    Automated Commit file: _update app/web/src/routes/menu-items/utilities.ts

--- app/web/src/routes/menu-items/utilities.ts
+++ app/web/src/routes/menu-items/utilities.ts
@@ -1,5 +1,5 @@
 // assets
-import { IconUsers, IconCode, IconMessage, IconCalendar } from "@tabler/icons";
+import { IconUsers, IconCode, IconMessage, IconCalendar } from '@tabler/icons-react';
 
 const icons = { IconUsers, IconCode, IconMessage, IconCalendar };
 
@@ -8,33 +8,33 @@ const icons = { IconUsers, IconCode, IconMessage, IconCalendar };
 // ==============================|| DEVS MENU ITEMS ||============================== //
 
 const utilities = {
-  id: "Devs",
-  title: "Devs",
-  type: "group",
-  roles: ["Ceo", "Developer"],
+  id: 'Devs',
+  title: 'Devs',
+  type: 'group',
+  roles: ['Ceo', 'Developer'],
 
   children: [
     {
-      id: "Hub",
-      title: "Dev Hub",
-      type: "item",
-      url: "/devs/hub",
+      id: 'Hub',
+      title: 'Dev Hub',
+      type: 'item',
+      url: '/devs/hub',
       icon: icons.IconCode,
       breadcrumbs: false,
     },
     {
-      id: "Interviews",
-      title: "Dev interviews",
-      type: "item",
-      url: "/devs/interviews",
+      id: 'Interviews',
+      title: 'Dev interviews',
+      type: 'item',
+      url: '/devs/interviews',
       icon: icons.IconCalendar,
       breadcrumbs: false,
     },
     {
-      id: "Devs",
-      title: "All devs",
-      type: "item",
-      url: "/devs/all",
+      id: 'Devs',
+      title: 'All devs',
+      type: 'item',
+      url: '/devs/all',
       icon: icons.IconUsers,
       breadcrumbs: false,
     },
