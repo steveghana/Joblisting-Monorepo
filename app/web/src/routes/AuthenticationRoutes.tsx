e601aada1ab7906990f62e8c0195c128c72c1d3b
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:26:39 +0000

    Automated Commit file: _update app/web/src/routes/AuthenticationRoutes.tsx

--- app/web/src/routes/AuthenticationRoutes.tsx
+++ app/web/src/routes/AuthenticationRoutes.tsx
@@ -3,38 +3,37 @@ import React from 'react';
 import MinimalLayout from '../layout/MinimalLayout';
 // project imports
 import Loadable from '../components/Loadable';
-import AuthCallBack from '../components/auth/login';
-import RoleAuth from '../components/auth/roleAuthForm';
+import AuthCallBack from '../components/auth/authCallback';
+import RoleAuth from '../views/authentication/roleAuthForm';
 // import MinimalLayout from 'layout/MinimalLayout';
 
 // login option 3 routing
-const AuthLogin3 = Loadable(lazy(() => import('../components/auth/authContainer/loginContainer')));
-const AuthRegister3 = Loadable(lazy(() => import('../components/auth/authContainer/registerContainer')));
+const AuthLogin3 = Loadable(lazy(() => import('../views/authentication/loginContainer')));
+const AuthRegister3 = Loadable(lazy(() => import('../views/authentication/registerContainer')));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
 const AuthenticationRoutes = {
-    path: '/',
-    element: <MinimalLayout />,
-    children: [
-        {
-            path: '/role/select',
-            element: <RoleAuth />,
-        },
-        {
-            path: '/auth/login',
-            element: <AuthLogin3 />,
-        },
-        {
-            path: '/auth/login/callback',
-            element: <AuthCallBack />,
-        },
-        {
-            path: '/auth/register',
-            element: <AuthRegister3 />,
-        },
-    ],
+  path: '/',
+  element: <MinimalLayout />,
+  children: [
+    {
+      path: '/role/select',
+      element: <RoleAuth />,
+    },
+    {
+      path: '/auth/login',
+      element: <AuthLogin3 />,
+    },
+    {
+      path: '/auth/login/callback',
+      element: <AuthCallBack />,
+    },
+    {
+      path: '/auth/register',
+      element: <AuthRegister3 />,
+    },
+  ],
 };
 
 export default AuthenticationRoutes;
-
