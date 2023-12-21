b7599d9bf03be20275f15b56cfb755ed0d204673
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:26:34 +0000

    Automated Commit file: _update app/web/src/layout/MainLayout/Sidebar/MenuList/NavCollapse/index.tsx

--- app/web/src/layout/MainLayout/Sidebar/MenuList/NavCollapse/index.tsx
+++ app/web/src/layout/MainLayout/Sidebar/MenuList/NavCollapse/index.tsx
@@ -1,27 +1,20 @@
-import PropTypes from "prop-types";
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router";
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router';
 
 // material-ui
-import { useTheme } from "@mui/material/styles";
-import {
-  Collapse,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import { useTheme } from '@mui/material/styles';
+import { Collapse, List, ListItemButton, ListItemIcon, ListItemText, Typography } from '@mui/material';
 
 // project imports
-import NavItem from "../NavItem";
+import NavItem from '../NavItem';
 
 // assets
-import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
-import { IconChevronDown, IconChevronUp } from "@tabler/icons";
-import { themePalette } from "../../../../../themes/schemes/palette";
-import { themeTypography } from "../../../../../themes/schemes/typography";
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { themePalette } from '../../../../../themes/schemes/palette';
+import { themeTypography } from '../../../../../themes/schemes/typography';
 // import themeTypography from "../../../../../themes/typography";
 // import { themeTypography } from "../../../../src/theme/schemes/typography";
 // themeTypography
@@ -38,7 +31,7 @@ const NavCollapse = ({ menu, level }) => {
   const handleClick = () => {
     setOpen(!open);
     setSelected(!selected ? menu.id : null);
-    if (menu?.id !== "authentication") {
+    if (menu?.id !== 'authentication') {
       navigate(menu.children[0]?.url);
     }
   };
@@ -75,9 +68,9 @@ const NavCollapse = ({ menu, level }) => {
   // menu collapse & item
   const menus = menu.children?.map((item) => {
     switch (item.type) {
-      case "collapse":
+      case 'collapse':
         return <NavCollapse key={item.id} menu={item} level={level + 1} />;
-      case "item":
+      case 'item':
         return <NavItem key={item.id} item={item} level={level + 1} />;
       default:
         return (
@@ -90,18 +83,14 @@ const NavCollapse = ({ menu, level }) => {
 
   const Icon = menu.icon;
   const menuIcon = menu.icon ? (
-    <Icon
-      strokeWidth={1.5}
-      size="1.3rem"
-      style={{ marginTop: "auto", marginBottom: "auto" }}
-    />
+    <Icon strokeWidth={1.5} size="1.3rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
   ) : (
     <FiberManualRecordIcon
       sx={{
         width: selected === menu.id ? 8 : 6,
         height: selected === menu.id ? 8 : 6,
       }}
-      fontSize={level > 0 ? "inherit" : "medium"}
+      fontSize={level > 0 ? 'inherit' : 'medium'}
     />
   );
 
@@ -111,52 +100,33 @@ const NavCollapse = ({ menu, level }) => {
         sx={{
           borderRadius: `${customization.borderRadius}px`,
           mb: 0.5,
-          alignItems: "flex-start",
-          backgroundColor: level > 1 ? "transparent !important" : "inherit",
+          alignItems: 'flex-start',
+          backgroundColor: level > 1 ? 'transparent !important' : 'inherit',
           py: level > 1 ? 1 : 1.25,
           pl: `${level * 24}px`,
         }}
         selected={selected === menu.id}
         onClick={handleClick}
       >
-        <ListItemIcon sx={{ my: "auto", minWidth: !menu.icon ? 18 : 36 }}>
-          {menuIcon}
-        </ListItemIcon>
+        <ListItemIcon sx={{ my: 'auto', minWidth: !menu.icon ? 18 : 36 }}>{menuIcon}</ListItemIcon>
         <ListItemText
           primary={
-            <Typography
-              variant={selected === menu.id ? "h5" : "body1"}
-              color="inherit"
-              sx={{ my: "auto" }}
-            >
+            <Typography variant={selected === menu.id ? 'h5' : 'body1'} color="inherit" sx={{ my: 'auto' }}>
               {menu.title}
             </Typography>
           }
           secondary={
             menu.caption && (
-              <Typography
-                variant="caption"
-                sx={{ ...themeTypography.subMenuCaption }}
-                display="block"
-                gutterBottom
-              >
+              <Typography variant="caption" sx={{ ...themeTypography.subMenuCaption }} display="block" gutterBottom>
                 {menu.caption}
               </Typography>
             )
           }
         />
         {open ? (
-          <IconChevronUp
-            stroke={1.5}
-            size="1rem"
-            style={{ marginTop: "auto", marginBottom: "auto" }}
-          />
+          <IconChevronUp stroke={1.5} size="1rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
         ) : (
-          <IconChevronDown
-            stroke={1.5}
-            size="1rem"
-            style={{ marginTop: "auto", marginBottom: "auto" }}
-          />
+          <IconChevronDown stroke={1.5} size="1rem" style={{ marginTop: 'auto', marginBottom: 'auto' }} />
         )}
       </ListItemButton>
       <Collapse in={open} timeout="auto" unmountOnExit>
@@ -164,14 +134,14 @@ const NavCollapse = ({ menu, level }) => {
           component="div"
           disablePadding
           sx={{
-            position: "relative",
-            "&:after": {
+            position: 'relative',
+            '&:after': {
               content: "''",
-              position: "absolute",
-              left: "32px",
+              position: 'absolute',
+              left: '32px',
               top: 0,
-              height: "100%",
-              width: "1px",
+              height: '100%',
+              width: '1px',
               opacity: 1,
               background: themePalette.primary.light,
             },
