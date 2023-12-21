25e26aee5121665c276b564b31abb24155d57d2b
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:25:55 +0000

    Automated Commit file: _update app/web/src/App.tsx

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import './styles/main.scss';
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 // import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
 // routing
@@ -13,46 +13,40 @@ import NavigationScroll from './layout/NavigationScroll';
 import { PureLightTheme } from './themes/schemes/PureLightTheme';
 // ==============================|| APP ||============================== //
 import Status500 from './views/status/Status500';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 const App = () => {
-    const queryClient = new QueryClient();
-
-    function fallbackRender({ error, resetErrorBoundary }) {
-        // Call resetErrorBoundary() to reset the error boundary and retry the render.
-        console.log(error.message);
-        return (
-            <>
-                <Status500 />
-            </>
-        );
-    }
-
+  function fallbackRender(props: FallbackProps) {
+    // Call resetErrorBoundary() to reset the error boundary and retry the render.
+    console.log(props.error.message);
     return (
-        <ErrorBoundary
-            fallbackRender={fallbackRender}
-            onReset={details => {
-                // Reset the state of your app so the error doesn't happen again
-            }}
-        >
-            <QueryClientProvider client={queryClient}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <StyledEngineProvider injectFirst>
-                        <ThemeProvider theme={PureLightTheme}>
-                            <CssBaseline />
-                            <NavigationScroll>
-                                <Routes />
-                            </NavigationScroll>
-                        </ThemeProvider>
-                    </StyledEngineProvider>
-                </LocalizationProvider>
-            </QueryClientProvider>
-            <ToastContainer />
-        </ErrorBoundary>
+      <>
+        <Status500 />
+      </>
     );
+  }
+
+  return (
+    <ErrorBoundary
+      fallbackRender={fallbackRender}
+      onReset={(details) => {
+        // Reset the state of your app so the error doesn't happen again
+      }}
+    >
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={PureLightTheme}>
+            <CssBaseline />
+            <NavigationScroll>
+              <Routes />
+            </NavigationScroll>
+          </ThemeProvider>
+        </StyledEngineProvider>
+      </LocalizationProvider>
+      <ToastContainer />
+    </ErrorBoundary>
+  );
 };
 
 export default App;
-
