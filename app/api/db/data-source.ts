ba928e29323bc9bb4c4f6bdd47c9398a0e15df23
steveghana <boatengstephen707@gmail.com>
2023-12-21 09:25:12 +0000

    Automated Commit file: _update app/api/db/data-source.ts

--- app/api/db/data-source.ts
+++ app/api/db/data-source.ts
@@ -5,18 +5,18 @@ import Entities from '../src/Config/model';
 import { TypeOrmLogger } from '../src/util/lg';
 
 import * as dotenv from 'dotenv';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 dotenv.config(); // load environment variables from .env file
 // const configService: ConfigService;
 
-export const dbConnexion = {
+export const dbConnetion: PostgresConnectionOptions = {
   type: 'postgres',
 
   host: process.env.DB_HOST || 'localhost',
   port: /*  parseInt(process.env.DB_PORT) || */ 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'postgres',
   logger: new TypeOrmLogger(),
   entities: [...Entities],
   // ssl: {
@@ -25,7 +25,8 @@ export const dbConnexion = {
 };
 
 const myDataSource = new DataSource({
-  ...dbConnexion,
+  ...dbConnetion,
+  database: process.env.DB_NAME || 'postgres',
   migrations: ['./dist/migrations/**/*.js'],
 } as DataSourceOptions);
 export default myDataSource;
