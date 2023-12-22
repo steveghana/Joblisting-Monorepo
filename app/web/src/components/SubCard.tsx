a8594b23a8d2772350978b3a4ba000fa80e09e5c
steveghana <boatengstephen707@gmail.com>
2023-12-22 11:05:03 +0000

    update commit bat file to accept commit messages

--- app/web/src/components/SubCard.tsx
+++ app/web/src/components/SubCard.tsx
@@ -1,19 +1,12 @@
-import React, { Ref } from "react";
-import PropTypes from "prop-types";
-import { forwardRef } from "react";
+import React, { Ref } from 'react';
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
 
 // material-ui
-import { useTheme } from "@mui/material/styles";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  Grid,
-  Typography,
-} from "@mui/material";
-import { themePalette } from "../themes/schemes/palette";
-
+import { useTheme } from '@mui/material/styles';
+import { Card, CardContent, CardHeader, Divider, Grid, Typography } from '@mui/material';
+import { themePalette } from '../themes/schemes/palette';
+import { motion } from 'framer-motion';
 // ==============================|| CUSTOM SUB CARD ||============================== //
 interface ISubCard {
   children: React.ReactNode;
@@ -27,73 +20,68 @@ interface ISubCard {
 }
 const SubCard: React.FC<ISubCard> = forwardRef(
   (
-    {
-      children,
-      content,
-      contentClass,
-      darkTitle,
-      secondary,
-      sx = {},
-      contentSX = {},
-      title,
-      ...others
-    },
-    ref: Ref<HTMLDivElement>
+    { children, content, contentClass, darkTitle, secondary, sx = {}, contentSX = {}, title, ...others },
+    ref: Ref<HTMLDivElement>,
   ) => {
     const theme = useTheme();
 
     return (
-      <Card
-        ref={ref}
-        sx={{
-          border: "2px solid",
-          borderColor: themePalette.primary.light,
-          ":hover": {
-            boxShadow: "0 4px 14px 0 rgb(32 40 45 / 8%)",
-          },
-          ...sx,
-        }}
-        // {...others}
+      <motion.div
+        key={window.location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
       >
-        {/* card header and action */}
-        {!darkTitle && title && (
-          <CardHeader
-            sx={{ p: 2 }}
-            title={<Typography variant="h5">{title}</Typography>}
-            // action={secondary}
-          />
-        )}
-        {darkTitle && title && (
-          <CardHeader
-            sx={{ p: 2 }}
-            title={<Typography variant="h4">{title}</Typography>}
-            // action={secondary}
-          />
-        )}
+        <Card
+          ref={ref}
+          sx={{
+            border: '2px solid',
+            borderColor: themePalette.primary.light,
+            ':hover': {
+              boxShadow: '0 4px 14px 0 rgb(32 40 45 / 8%)',
+            },
+            ...sx,
+          }}
+          // {...others}
+        >
+          {/* card header and action */}
+          {!darkTitle && title && (
+            <CardHeader
+              sx={{ p: 2 }}
+              title={<Typography variant="h5">{title}</Typography>}
+              // action={secondary}
+            />
+          )}
+          {darkTitle && title && (
+            <CardHeader
+              sx={{ p: 2 }}
+              title={<Typography variant="h4">{title}</Typography>}
+              // action={secondary}
+            />
+          )}
 
-        {/* content & header divider */}
-        {title && (
-          <Divider
-            sx={{
-              opacity: 1,
-              borderColor: themePalette.primary.light,
-            }}
-          />
-        )}
+          {/* content & header divider */}
+          {title && (
+            <Divider
+              sx={{
+                opacity: 1,
+                borderColor: themePalette.primary.light,
+              }}
+            />
+          )}
 
-        {/* card content */}
-        {content && (
-          <Grid
-            sx={{ p: { lg: 1.4, sm: 1, xs: 0.5 }, ...contentSX }}
-            className={contentClass || ""}
-          >
-            {children}
-          </Grid>
-        )}
-        {!content && children}
-      </Card>
+          {/* card content */}
+          {content && (
+            <Grid sx={{ p: { lg: 1.4, sm: 1, xs: 0.5 }, ...contentSX }} className={contentClass || ''}>
+              {children}
+            </Grid>
+          )}
+          {!content && children}
+        </Card>
+      </motion.div>
     );
-  }
+  },
 );
 
 SubCard.defaultProps = {
