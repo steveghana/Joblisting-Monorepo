8228a7d1b973e1c48a4bc7c2855c009060a836e7
steveghana <boatengstephen707@gmail.com>
2023-12-22 11:05:44 +0000

    update commit bat file to accept commit messages

--- commit_files.bat
+++ commit_files.bat
@@ -1,12 +1,15 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
+REM Prompt the user for a commit message
+set /p commitMessage=Enter the commit message: 
+
 for /f "tokens=*" %%i in ('git ls-files') do (
     set "file=%%i"
     set "filename=!file:%cd%\=!"
-    
+
     git add "%%i"
-    git commit -m "Automated Commit file: _update !filename!"
+    git commit -m "!commitMessage!"
 )
 
 git push
