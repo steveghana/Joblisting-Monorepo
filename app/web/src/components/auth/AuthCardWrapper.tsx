b26c4245ad36cc60716c57d403bd0d8683b3e706
steveghana <boatengstephen707@gmail.com>
2023-12-22 11:05:06 +0000

    update commit bat file to accept commit messages

--- app/web/src/components/auth/AuthCardWrapper.tsx
+++ app/web/src/components/auth/AuthCardWrapper.tsx
@@ -4,20 +4,19 @@ import MainCard from '../MainCard';
 // ==============================|| AUTHENTICATION CARD WRAPPER ||============================== //
 
 const AuthCardWrapper = ({ children, ...other }: { children: React.ReactNode }) => (
-    <MainCard
-        sx={{
-            margin: { xs: 2.5, md: 3 },
-            '& > *': {
-                flexGrow: 1,
-                flexBasis: '50%',
-            },
-        }}
-        content={false}
-        {...other}
-    >
-        <Box sx={{ p: { xs: 1, sm: 2, xl: 3 } }}>{children}</Box>
-    </MainCard>
+  <MainCard
+    sx={{
+      margin: { xs: 1.5, md: 2 },
+      '& > *': {
+        flexGrow: 1,
+        flexBasis: '50%',
+      },
+    }}
+    content={false}
+    {...other}
+  >
+    <Box sx={{ p: { xs: 1, sm: 1, xl: 2 } }}>{children}</Box>
+  </MainCard>
 );
 
 export default AuthCardWrapper;
-
