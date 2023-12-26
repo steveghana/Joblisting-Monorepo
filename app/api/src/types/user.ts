816fb30d6b3285568add1d2b7dc95d952bd4138f
steveghana <boatengstephen707@gmail.com>
2023-12-26 03:14:41 +0000

    Automated Commit [ app/api/src/types/user.ts

--- app/api/src/types/user.ts
+++ app/api/src/types/user.ts
@@ -2,6 +2,10 @@ export type IUser = {
   id?: string;
   email: string;
   password?: string;
+  names?: Record<string, any>[];
+  photos?: Record<string, any>[];
+  emailAddresses?: any[];
+  googleVerified?: boolean;
   role: IProfession;
   avatar?: string;
   firstName: string;
