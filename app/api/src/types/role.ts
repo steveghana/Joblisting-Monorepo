a5c376ee9273f4d0855707d3cf2dbf7650dbc65c
steveghana <boatengstephen707@gmail.com>
2023-12-26 03:14:41 +0000

    Automated Commit [ app/api/src/types/role.ts

--- app/api/src/types/role.ts
+++ app/api/src/types/role.ts
@@ -3,6 +3,7 @@ import { IApplication } from './application';
 import { IClient, IClientFormData } from './client';
 // import { IDeveloper } from './developer';
 import { Iinterviews } from './interviews';
+import { IDev } from './developer';
 
 export interface IRole {
   id?: string;
@@ -11,6 +12,7 @@ export interface IRole {
   devsNeeded: string;
   // link?:string
   aboutTheProject: string;
+  developers?: IDev[];
   title: string;
   methodology: string;
   experience: string;
