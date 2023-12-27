198df582a3fd3c57394bd6095d7795dd771b8b6a
steveghana <boatengstephen707@gmail.com>
2023-12-27 17:10:44 +0000

    Automated Commit file: _update app/web/src/components/NoData.tsx

--- app/web/src/components/NoData.tsx
+++ app/web/src/components/NoData.tsx
@@ -1,20 +1,25 @@
 import { Box, Grid, Typography } from '@mui/material';
-// import NoDAta from '@/assets/images/empty-box.png';
+import NoDAta from '@/assets/images/empty-box.png';
 
 const NoData = () => {
   return (
-    <Box
-      width={'100%'}
-      height={'100%'}
-      mt={2}
-      display={'flex'}
-      justifyContent={'center'}
-      alignItems={'center'}
-      flexDirection={'column'}
-    >
-      {/* <img width={150} height={150} src={NoDAta} alt="nodata" /> */}
-      <Typography variant="subtitle1">No data to display</Typography>
-    </Box>
+    <Grid height={'100%'} width={'100%'} sx={{ display: 'flex', alignSelf: 'center', overflow: 'hidden', p: 1 }}>
+      <Box
+        width={'100%'}
+        height={'100%'}
+        flexGrow={1}
+        minHeight={100}
+        mt={2}
+        alignContent={'stretch'}
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        flexDirection={'column'}
+      >
+        <img width={150} height={150} src={NoDAta} alt="nodata" />
+        <Typography variant="subtitle1">No data to display</Typography>
+      </Box>
+    </Grid>
   );
 };
 export default NoData;
