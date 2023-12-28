902e47639080ed2e44120f0c00d678ad2b56ad78
steveghana <boatengstephen707@gmail.com>
2023-12-28 14:35:57 +0000

    Automated Commit file: _update app/web/src/views/Devs/index.tsx

--- app/web/src/views/Devs/index.tsx
+++ app/web/src/views/Devs/index.tsx
@@ -7,9 +7,12 @@ import { useGetDevsQuery } from '../../store/services/dev.service';
 import MainCard from '../../components/MainCard';
 import { Grid } from '@mui/material';
 import TableSkeletonLoader from '@/components/Skeleton/tableSkeleton';
+import CustomButton from '@/components/button';
+import AddDevs from './Form/addDevs';
 
 const Developers = () => {
   const columns = useDevsColums();
+  const [open, setOpen] = React.useState(false);
 
   const { data: devs, isError, isLoading, isFetching, refetch } = useGetDevsQuery();
   const devsData = (devs?.length && devs?.filter(({ rolestatus }) => rolestatus === 'Accepted')) || [];
@@ -18,12 +21,29 @@ const Developers = () => {
     return <TableSkeletonLoader />;
   }
 
-  if (!devsData.length || isError) {
-    return <NoData />;
-  }
   return (
     <MainCard>
-      <DevTableData
+      <AddDevs open={open} onClose={() => setOpen(false)} />
+      {!devsData.length || isError ? (
+        <>
+          <CustomButton text="Add new Dev" onClick={() => setOpen(true)} />
+          <NoData />
+        </>
+      ) : (
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <DevTableData
+              devs={devsData}
+              isLoading={isLoading}
+              isFetching={isFetching}
+              isError={isError}
+              columns={columns}
+              refetch={refetch}
+            />
+          </Grid>
+        </Grid>
+      )}
+      {/* <DevTableData
         columns={columns}
         devs={devsData}
         // You can still use refetch if needed
@@ -31,7 +51,7 @@ const Developers = () => {
         isLoading={isLoading}
         isError={isError}
         isFetching={isFetching}
-      />
+      /> */}
     </MainCard>
   );
 };
