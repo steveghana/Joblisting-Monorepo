a49cf7ed54a560a20224f5446b6f009bed90f4f7
steveghana <boatengstephen707@gmail.com>
2023-12-28 20:06:37 +0000

    Automated Commit file: _update app/api/src/apps/api-gateway/exports.ts

--- app/api/src/apps/api-gateway/exports.ts
+++ app/api/src/apps/api-gateway/exports.ts
@@ -3,6 +3,7 @@ import { ApplicationsModule } from '../applications/applications.module';
 import { AuthModule } from '../auth/auth.module';
 import { ClientsModule } from '../clients/clients.module';
 import { ClockedHoursModule } from '../clocked-hours/clocked-hours.module';
+import { ContactModule } from '../Contact/contact.module';
 import { DevelopersModule } from '../developers/tests/developers.module';
 import { InterviewsModule } from '../interviews/interviews.module';
 import { RolesModule } from '../roles/roles.module';
@@ -18,4 +19,5 @@ export const Modules = [
   ApplicationsModule,
   RolesModule,
   ShortModule,
+  ContactModule,
 ];
