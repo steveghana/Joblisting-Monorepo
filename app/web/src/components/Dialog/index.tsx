f8766732f313943086e7ed9dc00a5359281e33fd
steveghana <boatengstephen707@gmail.com>
2023-12-29 09:42:21 +0000

    Automated Commit file: _update app/web/src/components/Dialog/index.tsx

--- app/web/src/components/Dialog/index.tsx
+++ app/web/src/components/Dialog/index.tsx
@@ -1,27 +1,22 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
-import { Close, LockOutlined, ThumbDown, ThumbUp } from "@mui/icons-material";
-import { Grid, IconButton, Typography } from "@mui/material";
-import CustomButton from "../button";
-import { MRT_Row } from "material-react-table";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Close, LockOutlined, ThumbDown, ThumbUp } from '@mui/icons-material';
+import { Grid, IconButton, Typography } from '@mui/material';
+import CustomButton from '../button';
+import { MRT_Row } from 'material-react-table';
 type IAlert<T> = {
   open: boolean;
   handleClose: () => void;
-  deleteFn: () => void;
+  deleteFn?: () => void; // TODO: Change prop to actionFn instead of deleteFn
 };
-export default function AlertDialog<T>({
-  handleClose,
-  open,
-  deleteFn,
-}: IAlert<T>) {
+export default function AlertDialog<T>({ handleClose, open, deleteFn }: IAlert<T>) {
   const proceed = () => {
-    // console.log("deltelfan start");
-    deleteFn();
+    deleteFn && deleteFn();
     handleClose();
   };
 
@@ -33,24 +28,15 @@ export default function AlertDialog<T>({
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle
-          display={"flex"}
-          alignItems={"center"}
-          position={"relative"}
-        >
-          <Grid
-            display={"flex"}
-            flexDirection={"column"}
-            alignItems={"center"}
-            mx={"auto"}
-          >
+        <DialogTitle display={'flex'} alignItems={'center'} position={'relative'}>
+          <Grid display={'flex'} flexDirection={'column'} alignItems={'center'} mx={'auto'}>
             <LockOutlined color="warning" />
             <Typography variant="h6" fontWeight={700}>
               Caution
             </Typography>
           </Grid>
           <IconButton
-            sx={{ position: "absolute", top: 1, right: 0 }}
+            sx={{ position: 'absolute', top: 1, right: 0 }}
             onClick={(e) => {
               e.stopPropagation();
               handleClose();
@@ -60,17 +46,17 @@ export default function AlertDialog<T>({
           </IconButton>
         </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            <Typography>Are you sure you want to delete this row.</Typography>
-            <Typography>Action taken is not reversible</Typography>
+          <DialogContentText component={'header'} id="alert-dialog-description" sx={{ textAlign: 'center' }}>
+            <Typography>Are you sure you want to perform this action.</Typography>
+            <Typography>Action taken might not be reversible</Typography>
           </DialogContentText>
         </DialogContent>
-        <DialogActions sx={{ display: "flex" }}>
+        <DialogActions sx={{ display: 'flex' }}>
           <Grid
             sx={{
-              display: "flex",
-              mx: "auto",
-              alignSelf: "center",
+              display: 'flex',
+              mx: 'auto',
+              alignSelf: 'center',
               gap: 1,
             }}
           >
