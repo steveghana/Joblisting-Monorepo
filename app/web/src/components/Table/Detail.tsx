807d059abe34ada4e8344e7ffde8e9bf25cecf7d
steveghana <boatengstephen707@gmail.com>
2023-12-29 09:42:28 +0000

    Automated Commit file: _update app/web/src/components/Table/Detail.tsx

--- app/web/src/components/Table/Detail.tsx
+++ app/web/src/components/Table/Detail.tsx
@@ -1,46 +1,37 @@
-import { Box, Chip, Grid, ListItemIcon, Typography } from "@mui/material";
-import { MRT_Row } from "material-react-table";
-import React from "react";
-import { IDev } from "../../types/devs";
-import { ApplicantsSubmission } from "../../types/roles";
-import { Email } from "./RowAction";
-import { Send } from "@mui/icons-material";
-import CustomButton from "../button";
+import { Box, Chip, Grid, ListItemIcon, Typography } from '@mui/material';
+import { MRT_Row } from 'material-react-table';
+import React from 'react';
+import { IDev } from '../../types/devs';
+import { ApplicantsSubmission } from '../../types/roles';
+import { Email } from './RowAction';
+import { Send } from '@mui/icons-material';
+import CustomButton from '../button';
 
 function TableDetail({ row }: { row: MRT_Row<ApplicantsSubmission> }) {
-  console.log(row, "this is the row");
   return (
     <Box
       sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
     >
       <Grid lg={6}>
-        <Typography variant="h6" component={"legend"}>
+        <Typography variant="h6" component={'legend'}>
           Skills:
         </Typography>
-        <Box display={"flex"} gap={1} my={1}>
+        <Box display={'flex'} gap={1} my={1}>
           {row?.original?.selectedSkills?.map((skill, i) => (
-            <Chip
-              key={i}
-              variant="outlined"
-              size="small"
-              color="default"
-              label={skill}
-            />
+            <Chip key={i} variant="outlined" size="small" color="default" label={skill} />
           ))}
         </Box>
         <ListItemIcon>
           <CustomButton text="Send email" endIcon={<Send />} />
         </ListItemIcon>
       </Grid>
-      <Grid maxWidth={"50%"} sx={{ textAlign: "left" }}>
+      <Grid maxWidth={'50%'} sx={{ textAlign: 'left' }}>
         <Typography variant="h6">Cover letter:</Typography>
-        <Typography variant="caption">
-          &quot;{row.original.coverLetter}&quot;
-        </Typography>
+        <Typography variant="caption">&quot;{row.original.coverLetter}&quot;</Typography>
       </Grid>
     </Box>
   );
