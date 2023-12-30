e4c42d3d1bb86b89d933141cf3bd8d36af82fa85
steveghana <boatengstephen707@gmail.com>
2023-12-30 07:44:53 +0000

    Automated Commit file: _update app/web/src/views/Devs/index.tsx

--- app/web/src/views/Devs/index.tsx
+++ app/web/src/views/Devs/index.tsx
@@ -4,15 +4,18 @@ import NoData from '../../components/NoData';
 import { useDevsColums } from '../../hooks/useAllDevsColumn';
 import { useGetDevsQuery } from '../../store/services/dev.service';
 import MainCard from '../../components/MainCard';
-import { Grid } from '@mui/material';
+import { Box, Grid, IconButton, Tooltip } from '@mui/material';
 import TableSkeletonLoader from '@/components/Skeleton/tableSkeleton';
 import CustomButton from '@/components/button';
 import AddDevs from './Form/addDevs';
 import DevTableData from './Tables/DevColumns';
+import { ArrowBackTwoTone } from '@mui/icons-material';
+import { useNavigate } from 'react-router';
 
 const Developers = () => {
   const columns = useDevsColums();
   const [open, setOpen] = React.useState(false);
+  const navigate = useNavigate();
 
   const { data: devs, isError, isLoading, isFetching, refetch } = useGetDevsQuery();
   const devsData =
@@ -21,12 +24,20 @@ const Developers = () => {
   if (isLoading || isFetching) {
     return <TableSkeletonLoader />;
   }
+  console.log(devs, 'from devs');
   return (
     <MainCard>
+      <Box display="flex" mb={3}>
+        <Tooltip arrow placement="top" onClick={() => navigate(-1)} title="Go back">
+          <IconButton color="primary" sx={{ p: 2, mr: 2 }}>
+            <ArrowBackTwoTone />
+          </IconButton>
+        </Tooltip>
+      </Box>
       <AddDevs open={open} onClose={() => setOpen(false)} />
+      <CustomButton text="Add new Dev" onClick={() => setOpen(true)} />
       {!devsData.length || isError ? (
         <>
-          <CustomButton text="Add new Dev" onClick={() => setOpen(true)} />
           <NoData />
         </>
       ) : (
