9184631667ce275336c736783490177ad19eb8f6
steveghana <boatengstephen707@gmail.com>
2023-12-30 07:45:02 +0000

    Automated Commit file: _update app/web/src/views/dashboard/Roles/index.tsx

--- app/web/src/views/dashboard/Roles/index.tsx
+++ app/web/src/views/dashboard/Roles/index.tsx
@@ -1,12 +1,15 @@
 import MainCard from '../../../components/MainCard';
-import { Grid } from '@mui/material';
+import { Box, Grid, IconButton, Tooltip } from '@mui/material';
 import { useGetRolesQuery } from '../../../store/services/role.service';
 import NoData from '../../../components/NoData';
 import RoleSkeleton from '@/components/Skeleton/roleSkeleton';
 import RoleCard from './components/roleCard';
+import { ArrowBackTwoTone } from '@mui/icons-material';
+import { useNavigate } from 'react-router';
 
 const Roles = () => {
   const { data, isLoading, isFetching, isError } = useGetRolesQuery();
+  const navigate = useNavigate();
 
   if (!data || !data.length) {
     return <NoData />;
@@ -17,6 +20,13 @@ const Roles = () => {
   const renderRoles = () => {
     return (
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2 }}>
+        <Box display="flex" mb={3} px={2}>
+          <Tooltip arrow placement="top" onClick={() => navigate(-1)} title="Go back">
+            <IconButton color="primary" sx={{ p: 2, mr: 2 }}>
+              <ArrowBackTwoTone />
+            </IconButton>
+          </Tooltip>
+        </Box>
         {data.map((role) => (
           <RoleCard key={role.id} role={role} />
         ))}
