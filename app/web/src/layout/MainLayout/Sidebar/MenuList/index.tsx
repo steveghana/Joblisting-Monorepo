9616feb14626c99bb636189b6da6063acad98f0b
steveghana <boatengstephen707@gmail.com>
2023-12-31 08:28:59 +0000

    Automated Commit file: _update app/web/src/layout/MainLayout/Sidebar/MenuList/index.tsx

--- app/web/src/layout/MainLayout/Sidebar/MenuList/index.tsx
+++ app/web/src/layout/MainLayout/Sidebar/MenuList/index.tsx
@@ -1,21 +1,19 @@
 // material-ui
-import { Typography } from "@mui/material";
-import React from "react";
+import { Typography } from '@mui/material';
+import React from 'react';
 // project imports
-import NavGroup from "./NavGroup";
-import menuItem from "../../../../routes/menu-items";
+import NavGroup from './NavGroup';
+import menuItem from '../../../../routes/menu-items';
 
 // ==============================|| SIDEBAR MENU LIST ||============================== //
 
-const MenuList = () => {
-  const userRole = sessionStorage.getItem("role");
-  const filteredSidebarData = menuItem.items.filter((item) =>
-    item.roles.includes(userRole)
-  );
+const MenuList = ({ navTitle }: { navTitle: (title: string) => void }) => {
+  const userRole = sessionStorage.getItem('role');
+  const filteredSidebarData = menuItem.items.filter((item) => item.roles.includes(userRole as string));
   const navItems = filteredSidebarData.map((item) => {
     switch (item.type) {
-      case "group":
-        return <NavGroup key={item.id} item={item} />;
+      case 'group':
+        return <NavGroup navTitle={(title) => navTitle(title)} key={item.id} item={item} />;
       default:
         return (
           <Typography key={item.id} variant="h6" color="error" align="center">
