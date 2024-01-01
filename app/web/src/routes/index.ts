a5985bbb94b39f14e0509af7415909b2bd5f5ca8
steveghana <boatengstephen707@gmail.com>
2024-01-01 16:11:10 +0000

    Automated Commit file: _update app/web/src/routes/index.ts

--- app/web/src/routes/index.ts
+++ app/web/src/routes/index.ts
@@ -1,8 +1,8 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes } from 'react-router-dom';
 
 // routes
-import MainRoutes, { ExternalRoutes, homeRoutes } from "./MainRoutes";
-import AuthenticationRoutes from "./AuthenticationRoutes";
+import MainRoutes, { homeRoutes } from './MainRoutes';
+import AuthenticationRoutes from './AuthenticationRoutes';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
