b1a90272db8cc1d7974a707dc43aafe9cacaf143
steveghana <boatengstephen707@gmail.com>
2024-01-01 16:11:13 +0000

    Automated Commit file: _update app/web/src/store/middleware/err.ts

--- app/web/src/store/middleware/err.ts
+++ app/web/src/store/middleware/err.ts
@@ -1,6 +1,6 @@
-import { isRejectedWithValue } from "@reduxjs/toolkit";
-import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
-import { toast } from "react-toastify";
+import { isRejectedWithValue } from '@reduxjs/toolkit';
+import type { MiddlewareAPI, Middleware } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 
 /**
  * Log a warning and show a toast!
@@ -11,23 +11,22 @@ export type IError = {
     timestamp: string;
   };
 };
-export const rtkQueryErrorLogger: Middleware =
-  (api: MiddlewareAPI) => (next) => (action) => {
-    // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
-    if (isRejectedWithValue(action)) {
-      console.warn("We got a rejected action!", action);
-      if (Array.isArray(action.payload)) {
-        (action.payload as any[]).map((item) =>
-          toast.error(item, {
-            position: "bottom-center",
-          })
-        );
-      } else {
-        toast.error(action.payload, {
-          position: "bottom-center",
-        });
-      }
+export const rtkQueryErrorLogger: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
+  // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
+  if (isRejectedWithValue(action)) {
+    console.warn('We got a rejected action!', action);
+    if (Array.isArray(action.payload)) {
+      (action.payload as any[]).map((item) =>
+        toast.error(item, {
+          position: 'bottom-center',
+        }),
+      );
+    } else {
+      toast.error(action.payload, {
+        position: 'bottom-center',
+      });
     }
+  }
 
-    return next(action);
-  };
+  return next(action);
+};
