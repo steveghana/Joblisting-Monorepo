668474b0cb6f929303b369bc40d5bb3c57476970
steveghana <boatengstephen707@gmail.com>
2024-01-01 16:08:45 +0000

    Automated Commit file: _update app/api/db/data-source.ts

--- app/api/db/data-source.ts
+++ app/api/db/data-source.ts
@@ -1,28 +1,19 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-// import config from '../src/apps/Config/config';
-// import { ConfigService } from '@nestjs/config';
 import Entities from '../src/Config/model';
 import { TypeOrmLogger } from '../src/util/lg';
-
 import * as dotenv from 'dotenv';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 dotenv.config(); // load environment variables from .env file
-// const configService: ConfigService;
-
 export const dbConnetion: PostgresConnectionOptions = {
   type: 'postgres',
-
   host: process.env.DB_HOST || 'localhost',
-  port: /*  parseInt(process.env.DB_PORT) || */ 5432,
+  port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
   logger: new TypeOrmLogger(),
   entities: [...Entities],
-  synchronize: process.env.NODE_ENV !== 'production',
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // }, // enable SSL/TLS
+  synchronize: process.env.NODE_ENV === 'development',
 };
 
 const myDataSource = new DataSource({
