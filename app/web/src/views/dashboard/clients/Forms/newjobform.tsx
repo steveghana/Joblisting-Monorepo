d83b2f99300effa6b7928a75e98b6e87c85754dd
steveghana <boatengstephen707@gmail.com>
2024-01-02 07:27:21 +0000

    Automated Commit file: _update app/web/src/views/dashboard/clients/Forms/newjobform.tsx

--- app/web/src/views/dashboard/clients/Forms/newjobform.tsx
+++ app/web/src/views/dashboard/clients/Forms/newjobform.tsx
@@ -1,10 +1,11 @@
 // NewJobForm.tsx
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, Grid } from '@mui/material';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import RoleInfo from './clientForm/addtionallInfo';
 import { useAddJobMutation } from '../../../../store/services/role.service';
 import { toast } from 'react-toastify';
+import { themePalette } from '@/themes/schemes/palette';
 
 interface NewJobFormProps {
   open: boolean;
@@ -36,7 +37,8 @@ const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose, roleId }) => {
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Add New Job</DialogTitle>
+      <DialogTitle sx={{ background: themePalette.warning.light }}>Add New Job</DialogTitle>
+
       <PerfectScrollbar
         component="div"
         style={{
@@ -45,9 +47,9 @@ const NewJobForm: React.FC<NewJobFormProps> = ({ open, onClose, roleId }) => {
           paddingRight: '16px',
         }}
       >
-        <DialogContent>
+        <Grid container sx={{ minWidth: '35dvw', p: 1 }}>
           <RoleInfo atClientPage={true} handleExternalSubmit={(values) => handleSubmit(values)} />
-        </DialogContent>
+        </Grid>
       </PerfectScrollbar>
     </Dialog>
   );
